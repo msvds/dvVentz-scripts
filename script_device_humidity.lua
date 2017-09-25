8887a036ff3501cae7b1641cc80f53f6c5d1dec8
msvds <martijnvanderschoot@gmail.com>
2017-09-25 21:16:07 +0200

    Added humidity script

---
+++ script_device_humidity.lua
@@ -0,0 +1,44 @@
+-- when motion, no motion, open or closed windows/door are detected, the counters are set to 0 here
+return {
+	active = true, -- set to false to disable this script
+	on = {
+		timer = {'every hour'}
+	},
+
+	execute = function(domoticz, device)
+		local hum_woonk = domoticz.devices(21)
+		local hum_k_lars = domoticz.devices(63)
+		local hum_buiten = domoticz.devices(60)
+        local MessageInterval = 3600 --Interval of sending email notifications
+        local lastMessageSent = uservariables["HumidityMessage"] --Time in system seconds when last message was sent
+        local hum_string
+        
+		domoticz.log(device.state)
+		if (hum_woonk.humidity > 70) then
+		    hum_string = "extreem"
+	    elseif (hum_woonk.humidity > 65) then
+		    hum_string = "heel erg"
+	    elseif (hum_woonk.humidity > 60) then
+		    hum_string = "erg"
+	    elseif (hum_woonk.humidity > 55) then
+		    hum_string = "redelijk"
+	    end
+		
+		if (hum_woonk.humidity > 55) then
+		    if (hum_buiten.humidity < hum_woonk.humidity) then
+		          domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid lager, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten kan helpen.",domoticz.PRIORITY_LOW)  
+		    elseif (hum_buiten.humidity > hum_woonk.humidity) then
+		          domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid hoger, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten helpt helaas niet.",domoticz.PRIORITY_LOW)  
+	        end
+		end
+	end
+}
+
+--function message (u)
+--    if (time > lastMessageSent + MessageInterval) then
+--        commandArray['SendNotification']=u
+--        os.execute ('/usr/local/bin/izsynth -e voicerss -v nl-nl -W 75 -t ' ..u)
+--        os.execute ('/usr/local/bin/izsynth -e voicerss -v nl-nl -W 75 -t ' ..u)
+--    end
+--    commandArray["Variable:HumidityMessage"] = tostring(time) --Last run time of the script is updated to Kamerplant1Message variable
+--end
