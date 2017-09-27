624f592d47f603b11382e04f85b013d56997ddf4
msvds <martijnvanderschoot@gmail.com>
2017-09-27 07:49:13 +0200

    Update script_device_humidity.lua
    removed messageinterval and adjusted timer

--- script_device_humidity.lua
+++ script_device_humidity.lua
@@ -2,7 +2,7 @@
 return {
 	active = true, -- set to false to disable this script
 	on = {
-		timer = {'every hour'}
+		timer = {'every 6 hours'}
 	},
 	data = {
             lastMessageSent = {initial=0}
@@ -11,29 +11,25 @@ return {
 		local hum_woonk = domoticz.devices(21)
 		local hum_k_lars = domoticz.devices(63)
 		local hum_buiten = domoticz.devices(60)
-        local MessageInterval = 6*60*60 --Interval of sending email notifications
         local hum_string
         
-		--domoticz.log(device.state)
-		if (domoticz.time > lastMessageSent + MessageInterval) then
-			if (hum_woonk.humidity > 70) then
-				hum_string = "extreem"
-			elseif (hum_woonk.humidity > 65) then
-				hum_string = "heel erg"
-			elseif (hum_woonk.humidity > 60) then
-				hum_string = "erg"
-			elseif (hum_woonk.humidity > 55) then
-				hum_string = "redelijk"
-			end
+		if (hum_woonk.humidity > 70) then
+			hum_string = "extreem"
+		elseif (hum_woonk.humidity > 65) then
+			hum_string = "heel erg"
+		elseif (hum_woonk.humidity > 60) then
+			hum_string = "erg"
+		elseif (hum_woonk.humidity > 55) then
+			hum_string = "redelijk"
+		end
 
-			if (hum_woonk.humidity > 55) then
-				if (hum_buiten.humidity < hum_woonk.humidity) then
-					domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid lager, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten kan helpen.",domoticz.PRIORITY_LOW)  
-					lastMessageSent = domoticz.time		
-				elseif (hum_buiten.humidity > hum_woonk.humidity) then
-					domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid hoger, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten helpt helaas niet.",domoticz.PRIORITY_LOW)  
-					lastMessageSent = domoticz.time	
-				end
+		if (hum_woonk.humidity > 55) then
+			if (hum_buiten.humidity < hum_woonk.humidity) then
+				domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid lager, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten kan helpen.",domoticz.PRIORITY_LOW)  
+				lastMessageSent = domoticz.time		
+			elseif (hum_buiten.humidity > hum_woonk.humidity) then
+				domoticz.notify('Vochtigheid hoog',"De vochtigheid in de woonkamer begint " ..hum_string .. " te worden, namelijk " ..tonumber(hum_woonk.humidity) .. ". Buiten is de vochtigheid hoger, namelijk " ..tonumber(hum_buiten.humidity) .." dus een raampje open zetten helpt helaas niet.",domoticz.PRIORITY_LOW)  
+				lastMessageSent = domoticz.time	
 			end
 		end
 	end
