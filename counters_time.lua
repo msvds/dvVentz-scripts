3ea399639580699845448f1a030801b3a5e4c8ef
msvds <martijnvanderschoot@gmail.com>
2017-10-02 14:57:51 +0200

    Update counters_time.lua
    Added NMC

--- counters_time.lua
+++ counters_time.lua
@@ -5,6 +5,21 @@ return {
 		['timer'] = 'every minute'
 	},
 	execute = function(domoticz)
-            domoticz.globalData.MC_Eetkamerdeur = domoticz.globalData.MC_Eetkamerdeur + 1
+		local Eetkamerdeur = domoticz.devices(25)
+		local Dakraamslaapk = domoticz.devices(81)
+		if (Eetkamerdeur == 'On') then
+			domoticz.globalData.MC_Eetkamerdeur = domoticz.globalData.MC_Eetkamerdeur + 1
+			domoticz.log('MC_Eetkamerdeur = ' ..domoticz.globalData.MC_Eetkamerdeur)
+		else
+			domoticz.globalData.NMC_Eetkamerdeur = domoticz.globalData.NMC_Eetkamerdeur + 1
+			domoticz.log('NMC_Eetkamerdeur = ' ..domoticz.globalData.NMC_Eetkamerdeur)
+		end
+		if (Dakraamslaapk == 'On') then
+			domoticz.globalData.MC_Dakraamslaapk = domoticz.globalData.MC_Dakraamslaapk + 1
+			domoticz.log('MC_Dakraamslaapk = ' ..domoticz.globalData.MC_Dakraamslaapk)
+		else
+			domoticz.globalData.NMC_Dakraamslaapk = domoticz.globalData.NMC_Dakraamslaapk + 1
+			domoticz.log('NMC_Dakraamslaapk = ' ..domoticz.globalData.NMC_Dakraamslaapk)
+		end
 	end
 }
