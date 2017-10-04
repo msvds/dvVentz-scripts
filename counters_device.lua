88ed73cecee1f0086dc198b37ef336ec0b2f4da2
msvds <martijnvanderschoot@gmail.com>
2017-10-04 08:22:45 +0200

    Update counters_device.lua

--- counters_device.lua
+++ counters_device.lua
@@ -3,11 +3,16 @@
 return {
 	active = true,
 	on = {
-		devices = {25,81},
+		devices = {25,81,83,107,116,153},
 	},
 	execute = function(domoticz)
 		local Eetkamerdeur = domoticz.devices(25)
 		local Dakraamslaapk = domoticz.devices(81)
+		local Balkondeurslaapk = domoticz.devices(83)
+		local Voordeur = domoticz.devices(107)
+		local BalkondeurNienke = domoticz.devices(116)
+		local Slaapkdeur = domoticz.devices(153)
+		local Dakraamzolder = domoticz.devices(85)
 		if (Eetkamerdeur == 'On') then
 			domoticz.globalData.MC_Eetkamerdeur = 0
 			domoticz.log('MC_Eetkamerdeur set to zero')
@@ -15,6 +20,26 @@ return {
 		if (Dakraamslaapk == 'On') then
 			domoticz.globalData.MC_Dakraamslaapk = 0
 			domoticz.log('MC_Dakraamslaapk set to zero')
+		end
+		if (Balkondeurslaapk == 'On') then
+			domoticz.globalData.MC_Balkondeurslaapk = 0
+			domoticz.log('MC_Balkondeurslaapk set to zero')
+		end
+		if (Voordeur == 'On') then
+			domoticz.globalData.MC_Voordeur = 0
+			domoticz.log('MC_Voordeur set to zero')
+		end
+		if (BalkondeurNienke == 'On') then
+			domoticz.globalData.MC_BalkondeurNienke = 0
+			domoticz.log('MC_BalkondeurNienke set to zero')
+		end
+		if (Slaapkdeur == 'On') then
+			domoticz.globalData.MC_Slaapkdeur = 0
+			domoticz.log('MC_Slaapkdeur set to zero')
+		end
+		if (Dakraamzolder == 'On') then
+			domoticz.globalData.MC_Dakraamzolder = 0
+			domoticz.log('MC_Dakraamzolder set to zero')
 		end
 	end
 }
