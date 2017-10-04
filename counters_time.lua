21de58f2c02e66da5c6b7e36ab17bd3d31aca063
msvds <martijnvanderschoot@gmail.com>
2017-10-04 08:35:44 +0200

    Update counters_time.lua

--- counters_time.lua
+++ counters_time.lua
@@ -12,6 +12,9 @@ return {
 		local BalkondeurNienke = domoticz.devices(116)
 		local Slaapkdeur = domoticz.devices(153)
 		local Dakraamzolder = domoticz.devices(85)
+		local PIR_woonk = domoticz.devices(23)		
+		local PIR_kamerLars = domoticz.devices(66)
+		local PIR_halboven = domoticz.devices(119)
 		if (Eetkamerdeur == 'On') then
 			domoticz.globalData.MC_Eetkamerdeur = domoticz.globalData.MC_Eetkamerdeur + 1
 			domoticz.log('MC_Eetkamerdeur = ' ..domoticz.globalData.MC_Eetkamerdeur)
@@ -32,34 +35,58 @@ return {
 		else
 			domoticz.globalData.NMC_Balkondeurslaapk = domoticz.globalData.NMC_Balkondeurslaapk + 1
 			domoticz.log('NMC_Balkondeurslaapk = ' ..domoticz.globalData.NMC_Balkondeurslaapk)
-		endend
+		end
 		if (Voordeur == 'On') then
 			domoticz.globalData.MC_Voordeur = domoticz.globalData.MC_Voordeur + 1
 			domoticz.log('MC_Voordeur = ' ..domoticz.globalData.MC_Voordeur)
 		else
 			domoticz.globalData.NMC_Voordeur = domoticz.globalData.NMC_Voordeur + 1
 			domoticz.log('NMC_Voordeur = ' ..domoticz.globalData.NMC_Voordeur)
-		endend
+		end
 		if (BalkondeurNienke == 'On') then
 			domoticz.globalData.MC_BalkondeurNienke = domoticz.globalData.MC_BalkondeurNienke + 1
 			domoticz.log('MC_BalkondeurNienke = ' ..domoticz.globalData.MC_BalkondeurNienke)
 		else
 			domoticz.globalData.NMC_BalkondeurNienke = domoticz.globalData.NMC_BalkondeurNienke + 1
 			domoticz.log('NMC_BalkondeurNienke = ' ..domoticz.globalData.NMC_BalkondeurNienke)
-		endend
+		end
 		if (Slaapkdeur == 'On') then
 			domoticz.globalData.MC_Slaapkdeur = domoticz.globalData.MC_Slaapkdeur + 1
 			domoticz.log('MC_Slaapkdeur = ' ..domoticz.globalData.MC_Slaapkdeur)
 		else
 			domoticz.globalData.NMC_Slaapkdeur = domoticz.globalData.NMC_Slaapkdeur + 1
 			domoticz.log('NMC_Slaapkdeur = ' ..domoticz.globalData.NMC_Slaapkdeur)
-		endend
+		end
 		if (Dakraamzolder == 'On') then
 			domoticz.globalData.MC_Dakraamzolder = domoticz.globalData.MC_Dakraamzolder + 1
 			domoticz.log('MC_Dakraamzolder = ' ..domoticz.globalData.MC_Dakraamzolder)
 		else
 			domoticz.globalData.NMC_Dakraamzolder = domoticz.globalData.NMC_Dakraamzolder + 1
 			domoticz.log('NMC_Dakraamzolder = ' ..domoticz.globalData.NMC_Dakraamzolder)
+		end
+		
+		if (PIR_woonk == 'On') then
+			domoticz.globalData.MC_PIR_woonk = domoticz.globalData.MC_PIR_woonk + 1
+			domoticz.log('MC_PIR_woonk = ' ..domoticz.globalData.MC_PIR_woonk)
+		else
+			domoticz.globalData.NMC_PIR_woonk = domoticz.globalData.NMC_PIR_woonk + 1
+			domoticz.log('NMC_PIR_woonk = ' ..domoticz.globalData.NMC_PIR_woonk)
+		end
+		
+		if (PIR_kamerLars  == 'On') then
+			domoticz.globalData.MC_PIR_kamerLars  = domoticz.globalData.MC_PIR_kamerLars + 1
+			domoticz.log('MC_PIR_kamerLars = ' ..domoticz.globalData.MC_PIR_kamerLars)
+		else
+			domoticz.globalData.NMC_PIR_kamerLars = domoticz.globalData.NMC_PIR_kamerLars + 1
+			domoticz.log('NMC_PIR_kamerLars = ' ..domoticz.globalData.NMC_PIR_kamerLars)
+		end
+		
+		if (PIR_halboven  == 'On') then
+			domoticz.globalData.MC_PIR_halboven  = domoticz.globalData.MC_PIR_halboven + 1
+			domoticz.log('MC_PIR_halboven = ' ..domoticz.globalData.MC_PIR_halboven)
+		else
+			domoticz.globalData.NMC_PIR_halboven = domoticz.globalData.NMC_PIR_halboven + 1
+			domoticz.log('NMC_PIR_halboven = ' ..domoticz.globalData.NMC_PIR_halboven)
 		end
 	end
 }
