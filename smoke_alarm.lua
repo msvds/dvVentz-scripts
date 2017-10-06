1ed764d6bba56c477ab70a143a29dca470e77b12
msvds <martijnvanderschoot@gmail.com>
2017-10-06 11:19:56 +0200

    Update smoke_alarm.lua
    Added light flashing

--- smoke_alarm.lua
+++ smoke_alarm.lua
@@ -5,8 +5,21 @@ return {
 		devices = {129}
     },
     execute = function(domoticz, SmokeAlarm)
-        if (SmokeAlarm.state == 'On') then
+        local lamp_boven_tv = domoticz.devices(13)
+		local lamp_spoelb_keuken = domoticz.devices(36)
+		local lamp_bank = domoticz.devices(15)
+		local schemerlamp_bank = domoticz.devices(16)
+		local schemerlamp_deur = domoticz.devices(97)		
+		local lamp_hal_boven = domoticz.devices(151)
+		if (SmokeAlarm.state == 'On') then
 			--domoticz.devices(27).state == 'Accident tone'
+			lamp_boven_tv.switchOn().forSec(1).repeatAfterSec(1, 10)
+			lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(1, 10)
+			lamp_bank.switchOn().forSec(1).repeatAfterSec(1, 10)
+			schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(1, 10)
+			schemerlamp_deur.switchOn().forSec(1).repeatAfterSec(1, 10)			
+			schemerlamp_deur.switchOn().forSec(1).repeatAfterSec(1, 10)
+			lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(1, 10)
 			domoticz.notify('Brand!', "De rookmelder in de hal boven gaat af" ,domoticz.PRIORITY_HIGH)  
         end
 	end
