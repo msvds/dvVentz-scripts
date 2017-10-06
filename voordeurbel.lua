4263275e62a56bcc4ae8a87000e82497bec7dd93
msvds <martijnvanderschoot@gmail.com>
2017-10-06 11:15:39 +0200

    Update voordeurbel.lua
    Added light flashing

--- voordeurbel.lua
+++ voordeurbel.lua
@@ -5,6 +5,10 @@ return {
 		devices = {131}
 	},
 	execute = function(domoticz, device)
+		local schemerlamp_deur = domoticz.devices(97)
+		local lamp_spoelb_keuken = domoticz.devices(36)
+		schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(1, 3)		
+		lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(1, 3)
 		domoticz.notify('De deurbel gaat',"de deurbel gaat, doe open!",domoticz.PRIORITY_LOW)  
 	end
 }
