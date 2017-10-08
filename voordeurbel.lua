ecdf80f27f3ceccbc38f1dffc5ea56297846ffc2
msvds <martijnvanderschoot@gmail.com>
2017-10-08 15:33:26 +0200

    Update voordeurbel.lua
    Added switching on voordeur lamp

--- voordeurbel.lua
+++ voordeurbel.lua
@@ -4,7 +4,7 @@ return {
 	active = true, -- set to false to disable this script
 	on = {
 		devices = {131
-		,91
+		--,91
 		}
 	},
 	execute = function(domoticz, device)
@@ -13,12 +13,16 @@ return {
 		local lamp_boven_tv = domoticz.devices(13)
 		local schemerlamp_bank = domoticz.devices(16)
 		local schemerlamp_deur = domoticz.devices(97)		
-		local lamp_hal_boven = domoticz.devices(151)		
-		schemerlamp_deur.switchOn().forSec(1).repeatAfterSec(2, 3)		
-		lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(2, 3)		
-		lamp_boven_tv.switchOn().forSec(1).repeatAfterSec(2, 3)		
-		schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(2, 3)				
-		lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(2, 3)
-		domoticz.notify('De deurbel gaat',"de deurbel gaat, doe open!",domoticz.PRIORITY_LOW)  
+		local lamp_hal_boven = domoticz.devices(151)
+		local lamp_voordeur = domoticz.devices(33)
+		schemerlamp_deur.switchOn().forSec(1).repeatAfterSec(1, 2)		
+		--lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(1, 3)		
+		lamp_boven_tv.switchOn().forSec(1).repeatAfterSec(1, 2)		
+		schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(1, 2)				
+		lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(1, 2)
+		domoticz.notify('De deurbel gaat',"de deurbel gaat, doe open!",domoticz.PRIORITY_LOW)		
+		if (IsDark == 'On') then
+			lamp_voordeur.switchOn()
+		end
 	end
 }
