384af438ea42f6dce8f648d40eedf8546358dea8
msvds <martijnvanderschoot@gmail.com>
2017-10-08 12:51:18 +0200

    Update voordeurbel.lua

--- voordeurbel.lua
+++ voordeurbel.lua
@@ -7,8 +7,15 @@ return {
 	execute = function(domoticz, device)
 		local schemerlamp_deur = domoticz.devices(97)
 		local lamp_spoelb_keuken = domoticz.devices(36)
-		schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(1, 3)		
-		lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(1, 3)
+		local lamp_boven_tv = domoticz.devices(13)
+		local schemerlamp_bank = domoticz.devices(16)
+		local schemerlamp_deur = domoticz.devices(97)		
+		local lamp_hal_boven = domoticz.devices(151)		
+		schemerlamp_deur.switchOn().forSec(1).repeatAfterSec(2, 3)		
+		lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(2, 3)		
+		lamp_boven_tv.switchOn().forSec(1).repeatAfterSec(2, 3)		
+		schemerlamp_bank.switchOn().forSec(1).repeatAfterSec(2, 3)				
+		lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(2, 3)
 		domoticz.notify('De deurbel gaat',"de deurbel gaat, doe open!",domoticz.PRIORITY_LOW)  
 	end
 }
