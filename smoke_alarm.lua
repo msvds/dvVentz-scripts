285d777fd496b2273d5332e78a718d1025bb6fc5
msvds <martijnvanderschoot@gmail.com>
2017-10-08 14:41:20 +0200

    Update smoke_alarm.lua
    added test switch

--- smoke_alarm.lua
+++ smoke_alarm.lua
@@ -1,9 +1,10 @@
 -- smoke alarm checks and notifications are set here
+-- test switch idx = 91
 return {
 	active = true, -- set to false to disable this script
 	on = {
-		devices = {129,197},
-	execute = function(domoticz, SmokeAlarm)
+		devices = {129,197,91},
+	execute = function(domoticz, device)
 		local smoke_alarm_zolder = domoticz.devices(129)
 		local smoke_alarm_keuken = domoticz.devices(197)
 		local lamp_boven_tv = domoticz.devices(13)
@@ -13,7 +14,7 @@ return {
 		local schemerlamp_bank = domoticz.devices(16)
 		local schemerlamp_deur = domoticz.devices(97)		
 		local lamp_hal_boven = domoticz.devices(151)
-		if (SmokeAlarm.state == 'On') then
+		if (device.state == 'On') then
 			--domoticz.devices(27).state == 'Accident tone'
 			lamp_boven_tv.switchOn().forSec(1).repeatAfterSec(1, 10)
 			lamp_spoelb_keuken.switchOn().forSec(1).repeatAfterSec(1, 10)
@@ -24,11 +25,14 @@ return {
 			lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(1, 10)			
 			domoticz.notify('Brand!', "Een rookmelder gaat af" ,domoticz.PRIORITY_HIGH)
 			domoticz.SOUND_SIREN
-			if (SmokeAlarm.name == 'Smoke Detector Zolder') then 
+			if (device.name == 'Smoke Detector Zolder') then 
 				domoticz.notify('Brand!', "De rookmelder in de hal boven gaat af" ,domoticz.PRIORITY_HIGH) 
 			end
-			if (SmokeAlarm.name == 'Smoke Detector Keuken') then 
+			if (device.name == 'Smoke Detector Keuken') then 
 				domoticz.notify('Brand!', "De rookmelder in de keuken boven gaat af" ,domoticz.PRIORITY_HIGH) 
+			end
+			if (device.name == 'Test Switch') then 
+				domoticz.notify('Brand!', "Dit was een test" ,domoticz.PRIORITY_HIGH) 
 			end
 		end
 	end
