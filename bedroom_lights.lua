497411c4ed5bb08aba7458c8f05e9d5b791b9704
msvds <martijnvanderschoot@gmail.com>
2017-10-08 15:24:00 +0200

    Create bedroom_lights.lua

---
+++ bedroom_lights.lua
@@ -0,0 +1,19 @@
+return {
+	active = true, -- set to false to disable this script
+	on = {
+		devices = {
+			153
+		},
+	},
+
+	execute = function(domoticz, device)
+		local dimmer_bed_martijn = domoticz.devices(149)
+		local dimmer_bed_suzanne = domoticz.devices(150)
+		local IsDark = domoticz.devices(78)
+		if (device.state == 'On' and IsDark == 'On') then
+			dimmer_bed_martijn.switchOn()
+			dimmer_bed_suzanne.switchOn()  
+			domoticz.log('Eetkamerdeur op terwijl het donker is -> Nachtlampjes aangezet')
+		end
+	end
+}
