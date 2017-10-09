c56332666dbce5097b0a30ac629ceb2c0dc27f01
msvds <martijnvanderschoot@gmail.com>
2017-10-09 10:58:27 +0200

    Update bedroom_lights.lua

--- bedroom_lights.lua
+++ bedroom_lights.lua
@@ -10,6 +10,7 @@ return {
 		local dimmer_bed_martijn = domoticz.devices(149)
 		local dimmer_bed_suzanne = domoticz.devices(150)
 		local IsDark = domoticz.devices(78)
+		domoticz.log('bedroom_lights.lua ' ..device.state.. ' and ' ..IsDark)
 		if (device.state == 'On' and IsDark == 'On') then
 			dimmer_bed_martijn.switchOn()
 			dimmer_bed_suzanne.switchOn()  
