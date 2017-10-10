06baea89f69108c94a5db090cada09fb6c3d8fcc
msvds <martijnvanderschoot@gmail.com>
2017-10-10 16:33:54 +0200

    Update bedroom_lights.lua

--- bedroom_lights.lua
+++ bedroom_lights.lua
@@ -10,11 +10,19 @@ return {
 		local dimmer_bed_martijn = domoticz.devices(149)
 		local dimmer_bed_suzanne = domoticz.devices(150)
 		local IsDark = domoticz.devices(78)
-		domoticz.log('bedroom_lights.lua ' ..device.state.. ' and ' ..IsDark.state)
-		if (device.state == 'Open' and IsDark.state == 'On') then
-			dimmer_bed_martijn.switchOn()
-			dimmer_bed_suzanne.switchOn()  
-			domoticz.log('Slaapkamerdeur open terwijl het donker is -> Nachtlampjes aangezet')
+		local Time = require('Time')
+		domoticz.log('bedroom_lights.lua ' ..device.state.. ' and ' ..IsDark.state)		
+		if (Time.matchesRule('at 19:00-00:30') and device.state == 'Open' and IsDark.state == 'On') then
+			-- between 19:00 and 0:30 then next day
+			if (dimmer_bed_martijn.state == 'Off') then
+				dimmer_bed_martijn.switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is -> Nachtlampjes aangezet')
+			end
+			if (dimmer_bed_martijn.state == 'Off') then
+				dimmer_bed_suzanne.switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is -> Nachtlampjes aangezet')
+			end
+			
 		end
 	end
 }
