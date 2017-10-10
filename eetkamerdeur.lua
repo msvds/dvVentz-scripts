7f793d4588063212371286223066ecb8a4a213f1
msvds <martijnvanderschoot@gmail.com>
2017-10-10 20:25:18 +0200

    Create eetkamerdeur.lua

---
+++ eetkamerdeur.lua
@@ -0,0 +1,22 @@
+return {
+	active = true, -- set to false to disable this script
+	on = {
+		devices = {
+			25
+		},
+	},
+
+	execute = function(domoticz, device)
+		local lampen_woonkamer = domoticz.devices(1)
+		local IsDark = domoticz.devices(78)
+		local SomeoneHome = domoticz.devices(96)
+		local Time = require('Time')
+		domoticz.log('eetkamerdeur.lua ' ..device.state.. ' and ' ..IsDark.state)		
+		if (device.state == 'Open' and IsDark.state == 'On' and SomeoneHome.state == 'Off') then
+			if (lampen_woonkamer.state == 'Off') then
+				lampen_woonkamer.switchOn()
+				domoticz.log('Eetkamerdeur open terwijl het donker is -> Nachtlampjes aangezet')
+			end		
+		end
+	end
+}
