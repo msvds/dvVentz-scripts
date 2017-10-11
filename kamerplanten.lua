805583abc094a5e3459da772f7b35f2a52ffe31f
msvds <martijnvanderschoot@gmail.com>
2017-10-11 07:19:52 +0200

    Update kamerplanten.lua
    changed from humidity to percentage

--- kamerplanten.lua
+++ kamerplanten.lua
@@ -13,46 +13,46 @@ return {
     	local message
 		
 		-- add new data
-		domoticz.data.plant1.add(hum_plant1.humidity)
-		domoticz.data.plant2.add(hum_plant2.humidity)
+		domoticz.data.plant1.add(hum_plant1.percentage)
+		domoticz.data.plant2.add(hum_plant2.percentage)
 
 		-- average over 28 items each 6 hours (1 week)
 		local average_humidities_plant1  = domoticz.data.plant1.avg()
 		local average_humidities_plant2  = domoticz.data.plant2.avg()    
     
 		if (hum_plant1.humidity < 25) then				
-        	message = message .. 'De plant in de hal boven begint heel erg droog te worden (<25%), namelijk ' ..tonumber(hum_plant1.humidity) .. '), '
+        	message = message .. 'De plant in de hal boven begint heel erg droog te worden (<25%), namelijk ' ..tonumber(hum_plant1.percentage) .. '), '
 		elseif (hum_plant1.humidity < 35) then				
-        	message = message .. 'De plant in de hal boven begint erg droog te worden (<35%), namelijk ' ..tonumber(hum_plant1.humidity) .. '), '
+        	message = message .. 'De plant in de hal boven begint erg droog te worden (<35%), namelijk ' ..tonumber(hum_plant1.percentage) .. '), '
 		elseif (hum_plant1.humidity < 40) then				
-        	message = message .. 'De plant in de hal boven begint droog te worden (<40%), namelijk ' ..tonumber(hum_plant1.humidity) .. '), '
+        	message = message .. 'De plant in de hal boven begint droog te worden (<40%), namelijk ' ..tonumber(hum_plant1.percentage) .. '), '
 		elseif (hum_plant1.humidity < 50) then				
-        	message = message .. 'De plant in de hal boven begint een beetje droog te worden (<50%), namelijk ' ..tonumber(hum_plant1.humidity) .. '), '
+        	message = message .. 'De plant in de hal boven begint een beetje droog te worden (<50%), namelijk ' ..tonumber(hum_plant1.percentage) .. '), '
 		end
     
-		if (hum_plant2.humidity < 10) then				
-			message = message .. 'De plant in de badkamer begint heel erg droog te worden (<25%), namelijk ' ..tonumber(hum_plant2.humidity) .. '), '
-		elseif (hum_plant2.humidity < 15) then				
-			message = message .. 'De plant in de badkamer begint erg droog te worden (<35%), namelijk ' ..tonumber(hum_plant2.humidity) .. '), '
-		elseif (hum_plant2.humidity < 20) then				
-			message = message .. 'De plant in de badkamer begint droog te worden (<40%), namelijk ' ..tonumber(hum_plant2.humidity) .. '), '
-		elseif (hum_plant2.humidity < 25) then				
-			message = message .. 'De plant in de badkamer begint een beetje droog te worden (<50%), namelijk ' ..tonumber(hum_plant2.humidity) .. '), '
+		if (hum_plant2.percentage < 10) then				
+			message = message .. 'De plant in de badkamer begint heel erg droog te worden (<25%), namelijk ' ..tonumber(hum_plant2.percentage) .. '), '
+		elseif (hum_plant2.percentage < 15) then				
+			message = message .. 'De plant in de badkamer begint erg droog te worden (<35%), namelijk ' ..tonumber(hum_plant2.percentage) .. '), '
+		elseif (hum_plant2.percentage < 20) then				
+			message = message .. 'De plant in de badkamer begint droog te worden (<40%), namelijk ' ..tonumber(hum_plant2.percentage) .. '), '
+		elseif (hum_plant2.percentage < 25) then				
+			message = message .. 'De plant in de badkamer begint een beetje droog te worden (<50%), namelijk ' ..tonumber(hum_plant2.percentage) .. '), '
 		end
 
-		if (average_humidities_plant1 - hum_plant1.humidity > 15) then
+		if (average_humidities_plant1 - hum_plant1.percentage > 15) then
 			message = message .. 'De plant in de hal boven drinkt veel, klopt het dat het zomer is?, '
-		elseif (average_humidities_plant1 - hum_plant1.humidity > 10) then
+		elseif (average_humidities_plant1 - hum_plant1.percentage > 10) then
 			message = message .. 'De plant in de hal boven drinkt redelijk veel, klopt het dat het lente is?, '
-		elseif (average_humidities_plant1 - hum_plant1.humidity < 10) then
+		elseif (average_humidities_plant1 - hum_plant1.percentage < 10) then
 			message = message .. 'De plant in de hal boven drinkt redelijk weinig, klopt het dat het winter of herfst is?, '
 		end
 
-		if (average_humidities_plant2 - hum_plant2.humidity > 5) then
+		if (average_humidities_plant2 - hum_plant2.percentage > 5) then
 			message = message .. 'De plant in de badkamer drinkt veel, klopt het dat het zomer is?, '
-		elseif (average_humidities_plant2 - hum_plant2.humidity > 3) then
+		elseif (average_humidities_plant2 - hum_plant2.percentage > 3) then
 			message = message .. 'De plant in de badkamer drinkt redelijk veel, klopt het dat het lente is?, '
-		elseif (average_humidities_plant2 - hum_plant2.humidity < 3) then
+		elseif (average_humidities_plant2 - hum_plant2.percentage < 3) then
 			message = message .. 'De plant in de badkamer drinkt redelijk weinig, klopt het dat het winter of herfst is?, '
 		end
 		
