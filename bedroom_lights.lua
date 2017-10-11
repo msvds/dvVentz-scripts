1f143776afd480c51fb9e2c6b518d90a3efde01e
msvds <martijnvanderschoot@gmail.com>
2017-10-11 07:22:26 +0200

    Update bedroom_lights.lua
    using domoticz.time.matchesRule

--- bedroom_lights.lua
+++ bedroom_lights.lua
@@ -10,10 +10,8 @@ return {
 		local dimmer_bed_martijn = domoticz.devices(149)
 		local dimmer_bed_suzanne = domoticz.devices(150)
 		local IsDark = domoticz.devices(78)
-		local Time = require('Time')
-		local t = Time('2016-12-12 07:35:00')
 		domoticz.log('bedroom_lights.lua ' ..device.state.. ' and ' ..IsDark.state)		
-		if (t.matchesRule('at 19:00-00:30') and device.state == 'Open' and IsDark.state == 'On') then
+		if (domoticz.time.matchesRule('at 19:00-00:30') and device.state == 'Open' and IsDark.state == 'On') then
 			-- between 19:00 and 0:30 then next day
 			if (dimmer_bed_martijn.state == 'Off') then
 				dimmer_bed_martijn.switchOn()
