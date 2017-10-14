c33ac7bc87293c55f6f3c499caac1211311e9e21
msvds <martijnvanderschoot@gmail.com>
2017-10-14 21:18:13 +0200

    Update counters_device.lua

--- counters_device.lua
+++ counters_device.lua
@@ -16,43 +16,44 @@ return {
 		local PIR_woonk = domoticz.devices(23)		
 		local PIR_kamerLars = domoticz.devices(66)
 		local PIR_halboven = domoticz.devices(119)
-		domoticz.log('Dakraamzolder.state' ..Dakraamzolder.state)
-		if (Eetkamerdeur.state == 'On') then
+		domoticz.log('Dakraamzolder.state: ' ..Dakraamzolder.state)
+		domoticz.log('PIR_halboven.state: ' ..PIR_halboven.state)
+		if (Eetkamerdeur.state == 'Open') then
 			domoticz.globalData.NMC_Eetkamerdeur = 0
 			domoticz.globalData.NMC_Floor1 = 0			  
 			domoticz.log('NMC_Eetkamerdeur & NMC_Floor1 set to zero')
 		end
-		if (Dakraamslaapk.state == 'On') then
+		if (Dakraamslaapk.state == 'Open') then
 			domoticz.globalData.NMC_Dakraamslaapk = 0
 			domoticz.globalData.NMC_Floor3 = 0
 			domoticz.log('NMC_Dakraamslaapk & NMC_Floor3 set to zero')
 		end
-		if (Balkondeurslaapk.state == 'On') then
+		if (Balkondeurslaapk.state == 'Open') then
 			domoticz.globalData.NMC_Balkondeurslaapk = 0
 			domoticz.globalData.NMC_Floor2 = 0
 			domoticz.log('NMC_Balkondeurslaapk & NMC_Floor2 set to zero')
 		end
-		if (Voordeur.state == 'On') then
+		if (Voordeur.state == 'Open') then
 			domoticz.globalData.NMC_Voordeur = 0
 			domoticz.globalData.NMC_Floor1 = 0
 			domoticz.log('NMC_Voordeur & NMC_Floor1 set to zero')
 		end
-		if (BalkondeurNienke.state == 'On') then
+		if (BalkondeurNienke.state == 'Open') then
 			domoticz.globalData.NMC_BalkondeurNienke = 0
 			domoticz.globalData.NMC_Floor2 = 0
 			domoticz.log('NMC_BalkondeurNienke & NMC_Floor2 set to zero')
 		end
-		if (Slaapkdeur.state == 'On') then
+		if (Slaapkdeur.state == 'Open') then
 			domoticz.globalData.NMC_Slaapkdeur = 0
 			domoticz.globalData.NMC_Floor2 = 0
 			domoticz.log('NMC_Slaapkdeur & NMC_Floor2 set to zero')
 		end
-		if (Dakraamzolder.state == 'On') then
+		if (Dakraamzolder.state == 'Open') then
 			domoticz.globalData.NMC_Dakraamzolder = 0
 			domoticz.globalData.NMC_Floor3 = 0
 			domoticz.log('NMC_Dakraamzolder & NMC_Floor3 set to zero')
 		end
-		if (PIR_woonk.state == 'On') then
+		if (PIR_woonk.state == 'Motion') then
 			domoticz.globalData.NMC_PIR_woonk = 0
 			domoticz.globalData.NMC_Floor1 = 0
 			domoticz.log('NMC_PIR_woonk & NMC_Floor1 set to zero')
