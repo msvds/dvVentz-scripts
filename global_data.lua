289afb9e54872f395791a639a0982593a264dd91
msvds <martijnvanderschoot@gmail.com>
2017-10-16 10:29:26 +0200

    Update global_data.lua

--- global_data.lua
+++ global_data.lua
@@ -60,7 +60,7 @@ return {
 				Counter = Counter + 1 -- increase the counter
 			    end
 			    return Counter -- always return the counter
-			end,0),
+			end,0)
 		end,
 		--CountersDevice =function(domoticz,DeviceIdx,DeviceType,DeviceGroup1,DeviceGroup2)
 		--	local Device = domoticz.devices(DeviceIdx)
