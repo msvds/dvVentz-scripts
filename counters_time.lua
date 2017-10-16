3fa27ffb6beb75b912195cae960e75a762fbd3ed
msvds <martijnvanderschoot@gmail.com>
2017-10-16 12:39:38 +0200

    Update counters_time.lua

--- counters_time.lua
+++ counters_time.lua
@@ -42,12 +42,14 @@ return {
 		--    return acc -- always return the accumulator
 		--end, 0)
 		--domoticz.globalData.OpenC_Eetkamerdeur = OpenC_Eetkamerdeur		
-		domoticz.log('domoticz.globalData.OpenC_Eetkamerdeur: ' ..domoticz.globalData.OpenC_Eetkamerdeur)
-		OpenC_Eetkamerdeur = domoticz.helpers.OpenC(domoticz, domoticz.devices(25), domoticz.globalData.OpenC_Eetkamerdeur)
-		domoticz.globalData.OpenC_Eetkamerdeur = OpenC_Eetkamerdeur		
-		domoticz.log('OpenC_Eetkamerdeur: ' .. OpenC_Eetkamerdeur)
-		domoticz.log('domoticz.globalData.ClosedC_Eetkamerdeur: ' ..domoticz.globalData.ClosedC_Eetkamerdeur)
-		OpenC_Eetkamerdeur = domoticz.helpers.ClosedC(domoticz, domoticz.devices(25), domoticz.globalData.ClosedC_Eetkamerdeur)
+		
+		--domoticz.log('domoticz.globalData.OpenC_Eetkamerdeur: ' ..domoticz.globalData.OpenC_Eetkamerdeur)
+		--OpenC_Eetkamerdeur = domoticz.helpers.OpenC(domoticz, domoticz.devices(25), domoticz.globalData.OpenC_Eetkamerdeur)
+		--domoticz.globalData.OpenC_Eetkamerdeur = OpenC_Eetkamerdeur		
+		--domoticz.log('OpenC_Eetkamerdeur: ' .. OpenC_Eetkamerdeur)
+		--domoticz.log('domoticz.globalData.ClosedC_Eetkamerdeur: ' ..domoticz.globalData.ClosedC_Eetkamerdeur)
+		--OpenC_Eetkamerdeur = domoticz.helpers.ClosedC(domoticz, domoticz.devices(25), domoticz.globalData.ClosedC_Eetkamerdeur)
+		
 		-- domoticz.globalData.ClosedC_Eetkamerdeur = ClosedC_Eetkamerdeur		
 		-- domoticz.log('ClosedC_Eetkamerdeur: ' .. ClosedC_Eetkamerdeur)
 		--if (Eetkamerdeur.state == 'Open') then
