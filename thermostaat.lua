c1c60cc54c704959d755a1a491c6c3ddc665391f
msvds <martijnvanderschoot@gmail.com>
2017-10-19 20:37:34 +0200

    Update thermostaat.lua

--- thermostaat.lua
+++ thermostaat.lua
@@ -6,8 +6,8 @@ return {
 	execute = function(domoticz, device)
 		local temp_buiten = domoticz.devices(59)
 		local Temperature_limit = '18'
-		local NMtimeout = '30'
-		local Opentimeout = '10'
+		local NM_timeout = '30'
+		local Open_timeout = '10'
 		local Time = require('Time')
 		debug = true
 		--ToonState = '50' -- Manual
@@ -43,11 +43,11 @@ return {
 			domoticz.helpers.changeSetPoint('12',' omdat het buiten warmer is dan ' ..Temperature_limit .. ' graden',false)
 		end		
 		-- If we have reached the timeout, disable the linked switches
-		if(domoticz.globalData.NMC_total >= tonumber(NMtimeout)) then
+		if (domoticz.globalData.NMC_total > tonumber(NM_timeout)) then
 			if debug then print('thermostaat.lua: domoticz.globalData.NMC_total: ' ..domoticz.globalData.NMC_total) end
 			domoticz.helpers.changeSetPoint('12','omdat de total no movement timout bereikt is',true)
 		end
-		if(domoticz.globalData.OpenC_Total >= tonumber(Opentimeout)) then
+		if (domoticz.globalData.OpenC_Total > tonumber(Open_timeout)) then
 			if debug then print('thermostaat.lua: domoticz.globalData.OpenC_Total: ' ..domoticz.globalData.OpenC_Total) end
 			domoticz.helpers.changeSetPoint('12','omdat de total open timout bereikt is',true)
 		end
