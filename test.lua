5f13b2044edc940d48425cb0768d0d55f310cc14
msvds <martijnvanderschoot@gmail.com>
2017-10-23 16:41:38 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -11,15 +11,17 @@ return {
 		local testswitch = domoticz.devices(91)
 		local dimmer_bed_martijn = domoticz.devices(149)		
 		local dimmer_bed_suzanne = domoticz.devices(150)
-		if (dimmer_bed_martijn.state == 'Off') then
-			--dimmer_bed_martijn.switchSelector(6)
-			dimmer_bed_martijn.switchOn()
-			domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
-		end
-		if (dimmer_bed_suzanne.state == 'Off') then
-			--dimmer_bed_suzanne.switchSelector(6)
-			dimmer_bed_suzanne.switchOn()
-			domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+		if (domoticz.time.matchesRule('at 16:00-00:30') and Slaapkdeur.state == 'Open') then
+			if (dimmer_bed_martijn.state == 'Off') then
+				--dimmer_bed_martijn.switchSelector(6)
+				dimmer_bed_martijn.switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+			end
+			if (dimmer_bed_suzanne.state == 'Off') then
+				--dimmer_bed_suzanne.switchSelector(6)
+				dimmer_bed_suzanne.switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+			end
 		end
 	end
 }
