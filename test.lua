080564dffa7a3c3791fb49e2c9d0877499f2e006
msvds <martijnvanderschoot@gmail.com>
2017-10-23 16:42:11 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -11,7 +11,7 @@ return {
 		local testswitch = domoticz.devices(91)
 		local dimmer_bed_martijn = domoticz.devices(149)		
 		local dimmer_bed_suzanne = domoticz.devices(150)
-		if (domoticz.time.matchesRule('at 16:00-00:30') and Slaapkdeur.state == 'Open') then
+		if (domoticz.time.matchesRule('at 16:00-00:30')) then --and Slaapkdeur.state == 'Open'
 			if (dimmer_bed_martijn.state == 'Off') then
 				--dimmer_bed_martijn.switchSelector(6)
 				dimmer_bed_martijn.switchOn()
