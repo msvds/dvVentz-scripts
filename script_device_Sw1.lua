7fe7fe8c550e3ab8c7280243646fa61b850a8244
msvds <martijnvanderschoot@gmail.com>
2017-10-30 14:18:25 +0100

    Update script_device_Sw1.lua
    Double Click action changed to check for departure

--- script_device_Sw1.lua
+++ script_device_Sw1.lua
@@ -34,8 +34,54 @@ return {
 			os.execute ('/usr/local/bin/izsynth -e voicerss -v nl-nl -W 75 -t "Alles is uitgeschakeld. Moet er nog een broodje gebakken worden? Weltrusten alvast!"')
 			domoticz.log('Lights turned off and Harmony turned off')
 		elseif (device.state == 'Double Click') then
-            radio.switchOn()
-			domoticz.log('Radio turned on')
+			--check everything is it's ready to departure
+			--check if windows are closed
+			local dakraamslaapkamer = domoticz.devices[81]
+			local dakraamzolderachter = domoticz.devices[85]
+			local Eetkamerdeur = domoticz.devices(25)
+			local Balkondeurslaapk = domoticz.devices(83)
+			local Voordeur = domoticz.devices(107)
+			local BalkondeurNienke = domoticz.devices(116)
+			local lampen_woonkamer = domoticz.groups(1)
+			local lamp_hal_boven = domoticz.devices(151)
+			local dimmer_bed_martijn = domoticz.devices(149)		
+			local dimmer_bed_suzanne = domoticz.devices(150)
+			local Schemerlamp_deur = domoticz.devices(97)
+			local Lamp_spoelb_keuken = domoticz.devices(36)
+			local MediaCenter = domoticz.devices(11)
+			local Televisie = domoticz.devices(7)
+			local Televisie_lage_resolutie = domoticz.devices(9)
+			if (dakraamslaapkamer.state == 'Open') then
+			   domoticz.notify('Dakraam slaapkamer is open',domoticz.PRIORITY_HIGH)
+			elseif (dakraamzolderachter.state == 'Open') then
+			   domoticz.notify('Dakraam zolder achter is open',domoticz.PRIORITY_HIGH)				
+			elseif (Eetkamerdeur.state == 'Open') then
+			   domoticz.notify('Eetkamerdeur is open',domoticz.PRIORITY_HIGH)
+			elseif (Balkondeurslaapk.state == 'Open') then
+			   domoticz.notify('Balkondeur slaapkamer is open',domoticz.PRIORITY_HIGH)
+			elseif (Voordeur.state == 'Open') then
+			   domoticz.notify('Voordeur slaapkamer is open',domoticz.PRIORITY_HIGH)
+			elseif (BalkondeurNienke.state == 'Open') then
+			   domoticz.notify('Balkondeur Nienke is open',domoticz.PRIORITY_HIGH)
+			elseif (lampen_woonkamer.state == 'On') then
+			   domoticz.notify('lampen woonkamer staan aan',domoticz.PRIORITY_HIGH)
+			elseif (lamp_hal_boven.state == 'On') then
+			   domoticz.notify('lamp hal boven staat aan',domoticz.PRIORITY_HIGH)
+			elseif (dimmer_bed_martijn.state == 'On') then
+			   domoticz.notify('dimmer bed martijn staat aan',domoticz.PRIORITY_HIGH)
+			elseif (dimmer_bed_suzanne.state == 'On') then
+			   domoticz.notify('dimmer bed suzanne staat aan',domoticz.PRIORITY_HIGH)
+			elseif (Schemerlamp_deur.state == 'On') then
+			   domoticz.notify('Schemerlamp deur staat aan',domoticz.PRIORITY_HIGH)
+			elseif (Lamp_spoelb_keuken.state == 'On') then
+			   domoticz.notify('Lamp spoelbak keuken staat aan',domoticz.PRIORITY_HIGH)
+			elseif (MediaCenter.state == 'On') then
+			   domoticz.notify('MediaCenter staat aan',domoticz.PRIORITY_HIGH)
+			elseif (Televisie.state == 'On') then
+			   domoticz.notify('Televisie staat aan',domoticz.PRIORITY_HIGH)
+			elseif (Televisie_lage_resolutie.state == 'On') then
+			   domoticz.notify('Televisie lage resolutie staat aan',domoticz.PRIORITY_HIGH)
+			end 
 		end
 	end
 }
