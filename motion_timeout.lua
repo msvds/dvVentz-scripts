100019f1c9d431b2e286615971d677669543b8e0
msvds <martijnvanderschoot@gmail.com>
2017-11-01 20:49:38 +0100

    Update motion_timeout.lua

--- motion_timeout.lua
+++ motion_timeout.lua
@@ -10,8 +10,8 @@ return {
 		local NM_timeout_floor2 = 10
 		local Time = require('Time')
 		local MediaCenter = domoticz.devices(11)
-		local Televisie = domoticz.devices(7)
-		local Televisie_lage_resolutie = domoticz.devices(9)
+		--local Televisie = domoticz.devices(7)
+		--local Televisie_lage_resolutie = domoticz.devices(9)
 		local lampen_woonkamer = domoticz.groups(1)
 		local lamp_hal_boven = domoticz.devices(151)
 		local dimmer_bed_martijn = domoticz.devices(149)		
@@ -26,7 +26,8 @@ return {
 		--domoticz.log('lamp_hal_boven.state = ' ..lamp_hal_boven.state)
 		--Do something when no movement timeout is reached
 		if (domoticz.globalData.NMC_Floor1 > NM_timeout_floor1) then
-			if (MediaCenter.state == 'Off' and Televisie.state == 'Off' and Televisie_lage_resolutie.state == 'Off') then
+			--if (MediaCenter.state == 'Off' and Televisie.state == 'Off' and Televisie_lage_resolutie.state == 'Off') then
+			if (MediaCenter.state == 'Off') then	
 				if (lampen_woonkamer.state == 'On') then
 					lampen_woonkamer.switchOff()
 					domoticz.log('No movement floor1 timeout is reached -> lampen woonkamer uit gezet', domoticz.LOG_INFO)
