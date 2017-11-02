79e882d08192baa915d7f2745795c83090959355
msvds <martijnvanderschoot@gmail.com>
2017-11-02 18:16:46 +0100

    Update door_bijkeuken.lua

--- door_bijkeuken.lua
+++ door_bijkeuken.lua
@@ -22,11 +22,6 @@ return {
 		local lamp_boven_tv = domoticz.devices(13)
 		local temperature_bijkeuken = domoticz.devices(110)
 		local temperature_woonk = domoticz.devices(20)
-		start_state_schemerlamp_deur = schemerlamp_deur.state
-		start_state_lamp_spoelb_keuken = lamp_spoelb_keuken.state
-		start_state_lamp_boven_tv = lamp_boven_tv.state
-		start_state_schemerlamp_bank = schemerlamp_bank.state
-		start_state_lamp_hal_boven = lamp_hal_boven.state
 		debug = false
 		if debug == true then 
 			domoticz.log('temperature_woonk.temperature = ' ..temperature_woonk.temperature)
@@ -36,37 +31,7 @@ return {
 		end
 		if ((temperature_woonk.temperature - temperature_bijkeuken.temperature > temp_diff) or (testing_on == true and test_switch.state == 'On'))  then
 			if ((domoticz.globalData.OpenC_Deurbijkeuken > Open_timeout_floor1 and domoticz.globalData.Open_timeout_message_interval > message_interval and Status_selector == '40') or (testing_on == true and test_switch.state == 'On')) then			
-				domoticz.notify('Bijkeukendeur open','Deur bijkeuken open voor ' ..domoticz.globalData.OpenC_Deurbijkeuken .. ' minuten terwijl het koud is in de bijkeuken (' ..domoticz.round(temperature_bijkeuken.temperature,1) ..' graden). Graag deur sluiten.', domoticz.LOG_INFO)
-				schemerlamp_deur.switchOn().forSec(3).repeatAfterSec(5, 3)		
-				lamp_spoelb_keuken.switchOn().forSec(3).repeatAfterSec(5, 3)		
-				lamp_boven_tv.switchOn().forSec(3).repeatAfterSec(5, 3)	
-				schemerlamp_bank.switchOn().forSec(3).repeatAfterSec(5, 3)				
-				lamp_hal_boven.switchOn().forSec(1).repeatAfterSec(5, 3)
-				if start_state_schemerlamp_deur == 'On' then
-					schemerlamp_deur.switchOn().afterSec(30)
-				else
-					schemerlamp_deur.switchOff().afterSec(30)
-				end
-				if start_state_lamp_spoelb_keuken == 'On' then
-					lamp_spoelb_keuken.switchOn().afterSec(30)
-				else
-					lamp_spoelb_keuken.switchOff().afterSec(30)
-				end
-				if start_state_lamp_boven_tv == 'On' then
-					lamp_boven_tv.switchOn().afterSec(30)
-				else
-					lamp_boven_tv.switchOff().afterSec(30)
-				end
-				if start_state_schemerlamp_bank == 'On' then
-					schemerlamp_bank.switchOn().afterSec(30)
-				else
-					schemerlamp_bank.switchOff().afterSec(30)
-				end
-				if start_state_lamp_hal_boven == 'On' then
-					lamp_hal_boven.switchOn().afterSec(30)
-				else
-					lamp_hal_boven.switchOff().afterSec(30)
-				end
+				sendnotification('Bijkeukendeur open','Deur bijkeuken open voor ' ..domoticz.globalData.OpenC_Deurbijkeuken .. ' minuten terwijl het koud is in de bijkeuken (' ..domoticz.round(temperature_bijkeuken.temperature,1) ..' graden). Graag deur sluiten.',schemerlamp_deur.state,lamp_spoelb_keuken.state,lamp_boven_tv.state,schemerlamp_bank.state,lamp_hal_boven.state,3,3,5)
 				domoticz.globalData.OpenC_Deurbijkeuken = 0
 			end
 		end
