e5d81eb2b00dcbb872a8961713b2e4682c6b0f85
msvds <martijnvanderschoot@gmail.com>
2017-11-03 08:02:32 +0100

    Update door_bijkeuken.lua

--- door_bijkeuken.lua
+++ door_bijkeuken.lua
@@ -13,7 +13,7 @@ return {
 		local Open_timeout_floor1 = 10
 		message_interval = 60
 		local Status_selector = domoticz.devices(90)
-		domoticz.globalData.Open_timeout_message_interval = domoticz.globalData.Open_timeout_message_interval + 1  
+		domoticz.globalData.door_bijkeuken_message_interval = domoticz.globalData.door_bijkeuken_message_interval + 1  
 		local Time = require('Time')
 		local lamp_hal_boven = domoticz.devices(151)
 		local schemerlamp_deur = domoticz.devices(97)
@@ -30,7 +30,7 @@ return {
 			domoticz.log('Open_timeout_floor1 = ' ..Open_timeout_floor1)
 		end
 		if ((temperature_woonk.temperature - temperature_bijkeuken.temperature > temp_diff) or (testing_on == true and test_switch.state == 'On'))  then
-			if ((domoticz.globalData.OpenC_Deurbijkeuken > Open_timeout_floor1 and domoticz.globalData.Open_timeout_message_interval > message_interval and Status_selector == '40') or (testing_on == true and test_switch.state == 'On')) then			
+			if ((domoticz.globalData.OpenC_Deurbijkeuken > Open_timeout_floor1 and domoticz.globalData.door_bijkeuken_message_interval > message_interval and Status_selector == '40') or (testing_on == true and test_switch.state == 'On')) then			
 				sendnotification('Bijkeukendeur open','Deur bijkeuken open voor ' ..domoticz.globalData.OpenC_Deurbijkeuken .. ' minuten terwijl het koud is in de bijkeuken (' ..domoticz.round(temperature_bijkeuken.temperature,1) ..' graden). Graag deur sluiten.',schemerlamp_deur.state,lamp_spoelb_keuken.state,lamp_boven_tv.state,schemerlamp_bank.state,lamp_hal_boven.state,3,3,5)
 				domoticz.globalData.OpenC_Deurbijkeuken = 0
 			end
