ea941d1d85bc29a4f4e284ca01627a0692a34c13
msvds <martijnvanderschoot@gmail.com>
2017-11-03 07:59:01 +0100

    Update door_garage.lua

--- door_garage.lua
+++ door_garage.lua
@@ -1,13 +1,31 @@
 -- Send a warning when the garage door has been open for more than 30 minutes
+-- Switch off when timeout is reached
+
 return {
-	active = true,
+	active = true, -- set to false to disable this script
 	on = {
-		['timer'] = 'every minute',
+		timer = {'every 10 minutes'},
+		devices = {91},
 	},
-	execute = function(domoticz)
-		local door = domoticz.devices['Garage deur']
-		if (door.state == 'Open' and door.lastUpdate.minutesAgo > 30) then
-			domoticz.notify('Garage deur alarm','De garage deur is voor meer dan 30 minuten open!',domoticz.PRIORITY_LOW)
+	execute = function(domoticz, device)
+		testing_on = false
+		local test_switch = domoticz.devices(91)
+		local Open_timeout_outside = 10
+		message_interval = 60
+		local Status_selector = domoticz.devices(90)
+		domoticz.globalData.door_garage_message_interval = domoticz.globalData.door_garage_message_interval + 1  
+		local Time = require('Time')
+		local lamp_hal_boven = domoticz.devices(151)
+		local schemerlamp_deur = domoticz.devices(97)
+		local lamp_spoelb_keuken = domoticz.devices(36)
+		local schemerlamp_bank = domoticz.devices(16)
+		local lamp_boven_tv = domoticz.devices(13)
+		local garagedoor = domoticz.devices['Garage deur']
+		debug = false
+		if (garagedoor.state == 'Open' and door.lastUpdate.minutesAgo > 30) then
+			sendnotification('Garage deur alarm','De garage deur is voor meer dan 30 minuten open!',schemerlamp_deur.state,lamp_spoelb_keuken.state,lamp_boven_tv.state,schemerlamp_bank.state,lamp_hal_boven.state,3,3,5)
+			domoticz.globalData.OpenC_Deurgarage = 0
 		end
 	end
 }
+
