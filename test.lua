844306e91c31e57baf5b002ba6a8db801de84936
msvds <martijnvanderschoot@gmail.com>
2017-11-09 14:56:18 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 return {
-	active = false,
+	active = true,
 	on = {
 		devices = {
 			91
@@ -9,19 +9,23 @@ return {
 		debug = true
 		domoticz.log('Testswitch', domoticz.LOG_INFO)
 		local testswitch = domoticz.devices(91)
-		local dimmer_bed_martijn = domoticz.devices(149)		
-		local dimmer_bed_suzanne = domoticz.devices(150)
-		if (domoticz.time.matchesRule('at 16:00-00:30')) then --and Slaapkdeur.state == 'Open'
-			if (dimmer_bed_martijn.state == 'Off') then
-				dimmer_bed_martijn.dimTo(20)
-				--dimmer_bed_martijn.switchOn()
-				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
-			end
-			if (dimmer_bed_suzanne.state == 'Off') then
-				dimmer_bed_suzanne.dimTo(20)
-				--dimmer_bed_suzanne.switchOn()
-				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
-			end
-		end
+		local Status_selector = domoticz.devices(90)
+		domoticz.log(Status_selector.state)
+		Status_selector.switchSelector(30)
+		domoticz.log(Status_selector.state)
+		--local dimmer_bed_martijn = domoticz.devices(149)		
+		--local dimmer_bed_suzanne = domoticz.devices(150)
+		--if (domoticz.time.matchesRule('at 16:00-00:30')) then --and Slaapkdeur.state == 'Open'
+		--	if (dimmer_bed_martijn.state == 'Off') then
+		--		dimmer_bed_martijn.dimTo(20)
+		--		--dimmer_bed_martijn.switchOn()
+		--		domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+		--	end
+		--	if (dimmer_bed_suzanne.state == 'Off') then
+		--		dimmer_bed_suzanne.dimTo(20)
+		--		--dimmer_bed_suzanne.switchOn()
+		--		domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+		--	end
+		--e--n-d
 	end
 }
