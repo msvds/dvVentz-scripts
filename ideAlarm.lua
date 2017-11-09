e3e5f11ef287eadd4358f53c803188362f410566
msvds <martijnvanderschoot@gmail.com>
2017-11-09 22:31:29 +0100

    Create ideAlarm.lua

---
+++ ideAlarm.lua
@@ -0,0 +1,35 @@
+--[[
+ideAlarm.lua
+
+Please read: https://github.com/allan-gam/ideAlarm/wiki
+
+Do not change anything in this file.
+--]]
+
+local alarm = require "ideAlarmModule"
+
+local triggerDevices = alarm.triggerDevices()
+
+local data = {}
+data['nagEvent'] = {history = true, maxItems = 1}
+for i = 1, alarm.qtyAlarmZones() do
+	data['nagZ'..tostring(i)] = {initial=0}
+end
+
+return {
+	active = true,
+	logging = {
+		level = alarm.loggingLevel(domoticz), -- Can be set in the configuration file
+		marker = alarm.version()
+	},
+	on = {
+		devices = triggerDevices,
+		security = {domoticz.SECURITY_ARMEDAWAY, domoticz.SECURITY_ARMEDHOME, domoticz.SECURITY_DISARMED},
+		timer = alarm.timerTriggers()
+	},
+	data = data,
+	execute = function(domoticz, device, triggerInfo)
+		domoticz.log('Triggered by '..((device) and 'device: '..device.name..', device state is: '..device.state or 'Domoticz Security'), domoticz.LOG_DEBUG)
+		alarm.execute(domoticz, device, triggerInfo)
+	end
+}
