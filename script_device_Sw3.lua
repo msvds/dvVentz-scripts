6d8226fe2359443fc5804b92ebb86ac4496a6bb7
msvds <martijnvanderschoot@gmail.com>
2017-11-11 21:49:19 +0100

    Update script_device_Sw3.lua

--- script_device_Sw3.lua
+++ script_device_Sw3.lua
@@ -7,6 +7,9 @@ return {
 	},
 
 	execute = function(domoticz, device)
-		domoticz.helpers.gotosleep(domoticz)
+		domoticz.devices('Status').switchSelector(30) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+		domoticz.helpers.switch_all_lights_off(domoticz)
+		domoticz.helpers.check_doors_and_windows(domoticz)
+		domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false,currentSetpoint)
 	end
 }
