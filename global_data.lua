de125057363eb3be26931d0ca6fdafced0647228
msvds <martijnvanderschoot@gmail.com>
2017-11-11 21:14:51 +0100

    Update global_data.lua

--- global_data.lua
+++ global_data.lua
@@ -65,14 +65,14 @@ return {
 				domoticz.log('Huidige programma Toon veranderd naar '.. newState .. ' ' ..reason)
 			end
 		end,
-		getcurrentSetpoint =function(domoticz)
+		currentSetpoint =function(domoticz)
 			local json = assert(loadfile "/home/pi/domoticz/scripts/lua/JSON.lua")()  -- For Linux (LEDE)
 			local handle = assert(io.popen(string.format('curl -m 5 http://%s/happ_thermstat?action=getThermostatInfo', '192.168.178.183')))
 			local ThermostatInfo = handle:read('*all')
 			handle:close()
 			local jsonThermostatInfo = json:decode(ThermostatInfo)
 			if jsonThermostatInfo ~= nil then
-				local currentSetpoint = tonumber(jsonThermostatInfo.currentSetpoint) / 100
+				currentSetpoint = tonumber(jsonThermostatInfo.currentSetpoint) / 100
 				domoticz.log('A Huidige setpoint is '.. currentSetpoint) 
 			end
 			return currentSetpoint
