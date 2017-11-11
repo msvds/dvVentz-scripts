8ddf3050b213ecca268063e7830c92670116c4b2
msvds <martijnvanderschoot@gmail.com>
2017-11-11 15:28:27 +0100

    Create SomeOneHome.lua

---
+++ SomeOneHome.lua
@@ -0,0 +1,17 @@
+return {
+	active = true, -- set to false to disable this script
+	on = {
+		devices = {
+			'Suzanne smartphone',
+			'Martijn smartphone'
+		},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('SomeoneHome') == 'Off' and (domoticz.devices('Suzanne smartphone') == 'On' or domoticz.devices('Martijn smartphone') == 'On') then
+			domoticz.devices('SomeoneHome').switchOn()
+		elseif (domoticz.devices('SomeoneHome') == 'On' and (domoticz.devices('Suzanne smartphone') == 'Of' and domoticz.devices('Martijn smartphone') == 'Off') then
+			domoticz.devices('SomeoneHome').switchOff()
+		end		
+	end
+}
