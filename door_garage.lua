07513dcf58d356e54d3ca15977576d2258303de4
msvds <martijnvanderschoot@gmail.com>
2017-11-11 10:36:45 +0100

    Update door_garage.lua

--- door_garage.lua
+++ door_garage.lua
@@ -5,29 +5,20 @@ return {
 	active = true, -- set to false to disable this script
 	on = {
 		timer = {'every 10 minutes'},
-		devices = {91},
+		devices = {'Test Switch'},
 	},
 	execute = function(domoticz, device)
 		testing_on = false
-		local test_switch = domoticz.devices(91)
 		local Open_timeout_outside = 10
 		message_interval = 60
-		local Status_selector = domoticz.devices(90)
 		domoticz.globalData.door_garage_message_interval = domoticz.globalData.door_garage_message_interval + 1  
 		local Time = require('Time')
-		local lamp_hal_boven = domoticz.devices(151)
-		local schemerlamp_deur = domoticz.devices(97)
-		local lamp_spoelb_keuken = domoticz.devices(36)
-		local schemerlamp_bank = domoticz.devices(16)
-		local lamp_boven_tv = domoticz.devices(13)
-		local Deurgarage = domoticz.devices(105)
 		debug = false
-		if (Deurgarage.state == 'Open' and Deurgarage.lastUpdate.minutesAgo > 30) then
-			if ((domoticz.globalData.OpenC_Deurgarage > Open_timeout_outside and domoticz.globalData.door_garage_message_interval > message_interval) or (test == true and test_switch.state == 'On')) then
-				domoticz.helpers.sendnotification(domoticz,'Garage deur alarm','De garage deur is voor meer dan 30 minuten open!',schemerlamp_deur.state,lamp_spoelb_keuken.state,lamp_boven_tv.state,schemerlamp_bank.state,lamp_hal_boven.state,3,3,5)
-				domoticz.globalData.OpenC_Deurgarage = 0
+		if (domoticz.devices('Garage deur').state == 'Open' and domoticz.devices('Garage deur').lastUpdate.minutesAgo > 30) then
+			if ((domoticz.globalData.OpenC_domoticz.devices('Garage deur') > Open_timeout_outside and domoticz.globalData.door_garage_message_interval > message_interval) or (test == true and domoticz.devices('Test Switch').state == 'On')) then
+				domoticz.helpers.sendnotification(domoticz,'Garage deur alarm','De garage deur is voor meer dan 30 minuten open!',domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Lamp boven TV').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,3,3,5)
+				domoticz.globalData.OpenC_domoticz.devices('Garage deur') = 0
 			end
 		end
 	end
 }
-
