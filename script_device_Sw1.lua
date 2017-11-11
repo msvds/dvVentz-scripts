49d98463af8e7681ef43fbcd00d2143f8eac13c9
msvds <martijnvanderschoot@gmail.com>
2017-11-11 21:34:58 +0100

    Update script_device_Sw1.lua

--- script_device_Sw1.lua
+++ script_device_Sw1.lua
@@ -51,7 +51,8 @@ return {
 			schemerlamp_bank.switchOff()
 			schemerlamp_deur.switchOff()
             		--harmony_poweroff.switchOn()
-			domoticz.helpers.changeToonSceneComplete(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false)
+			local currentSetpoint = domoticz.helpers.currentSetpoint(domoticz)
+			domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false,currentSetpoint)
 			os.execute ('/usr/local/bin/izsynth -e voicerss -v nl-nl -W 75 -t "Alles is uitgeschakeld. Moet er nog een broodje gebakken worden? Weltrusten alvast!"')
 			domoticz.log('Lights turned off and Harmony turned off')
 			if (dakraamslaapkamer.state == 'Open') then
