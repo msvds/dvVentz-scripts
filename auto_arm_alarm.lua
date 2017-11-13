2e2d1f2f6c1a1cd068dc86d69a6f7dbe0caf0dbd
msvds <martijnvanderschoot@gmail.com>
2017-11-13 11:34:25 +0100

    Update auto_arm_alarm.lua

--- auto_arm_alarm.lua
+++ auto_arm_alarm.lua
@@ -9,7 +9,7 @@ return {
 	execute = function(domoticz, device)
 		local alarm = require "ideAlarmModule"
 		if (device.name == 'SomeoneHome' and device.state == 'On') then
-			alarm.zones('My Home').disArmZone(domoticz) -- This will disarm the zone "My Home"
+			--alarm.zones('My Home').disArmZone(domoticz) -- This will disarm the zone "My Home"
 		elseif (device.name == 'SomeoneHome' and device.state == 'Off') then
 			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDAWAY) -- This will  the zone "My Home" to "Armed Away" after the default exit delay
 		end
