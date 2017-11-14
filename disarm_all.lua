94fbc2be2e68758cd525f2e37fdec42513cc16f8
msvds <martijnvanderschoot@gmail.com>
2017-11-14 22:18:24 +0100

    Update disarm_all.lua

--- disarm_all.lua
+++ disarm_all.lua
@@ -2,11 +2,11 @@ return {
 	active = false,
 	on = {
 		devices = {
-			'DISARM ALL ZONES' -- You'd need a switch named like this for this example to work
+			'Sw4' -- You'd need a switch named like this for this example to work
 		}
 	},
 	execute = function(domoticz, device)
-		if device.state == 'On' then
+		if device.state == 'Click' then
 			local alarm = require "ideAlarmModule"
 			for i=1, alarm.qtyAlarmZones() do
 				alarm.zones(i).disArmZone(domoticz)
