02d91cfbd7303216ae1848bca26d0ba31b2e2e10
msvds <martijnvanderschoot@gmail.com>
2017-11-20 20:28:59 +0100

    Update auto_arm_alarm.lua

--- auto_arm_alarm.lua
+++ auto_arm_alarm.lua
@@ -15,7 +15,7 @@ return {
 			if alarm.zones('My Home').armingMode(domoticz) ~= nil then domoticz.log(alarm.zones('My Home').armingMode(domoticz)) end
 			if domoticz.SECURITY_DISARMED ~= nil then domoticz.log(domoticz.SECURITY_DISARMED) end
 			if domoticz.time.hours ~= nil then domoticz.log(domoticz.time.hours) end
-			if (alarm.zones('My Home').armingMode(domoticz) ~= domoticz.SECURITY_DISARMED and domoticz.time.matchesRule('at 6:00-23:00') then
+			if (alarm.zones('My Home').armingMode(domoticz) ~= domoticz.SECURITY_DISARMED and domoticz.time.matchesRule('at 6:00-23:00')) then
 				alarm.zones('My Home').disArmZone(domoticz)
 			end
 			--alarm.zones('My Home').disArmZone(domoticz) -- This will disarm the zone "My Home"
