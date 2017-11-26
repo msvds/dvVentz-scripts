63673c3a5df8cfc00750633f0421b754157e04c7
msvds <martijnvanderschoot@gmail.com>
2017-11-26 09:23:02 +0100

    Update script_device_Sw3.lua

--- script_device_Sw3.lua
+++ script_device_Sw3.lua
@@ -17,7 +17,8 @@ return {
 			domoticz.devices('Status').switchSelector(30) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 			domoticz.helpers.switch_all_lights_off(domoticz)
 			domoticz.helpers.check_doors_and_windows(domoticz)
-			domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false,currentSetpoint)
+			domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
+			domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
 			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDHOME) -- This will  the zone "My Home" to "Armed Home" after the default exit delay
 		end		
 	end
