080aa3e3dabe7ee57150d54ac652c6644466a4c9
msvds <martijnvanderschoot@gmail.com>
2017-11-29 08:21:52 +0100

    Update script_device_Sw2.lua

--- script_device_Sw2.lua
+++ script_device_Sw2.lua
@@ -2,22 +2,29 @@ return {
 	active = true, -- set to false to disable this script
 	on = {
 		devices = {
-			'Sw2'
+			'Sw2' -- Switch chill kamer
 		},
 	},
 
 	execute = function(domoticz, device)
-		if (device.state == 'Double Click') then
-			domoticz.devices('Lampen logeerkamer').switchOff()
+		local alarm = require "ideAlarmModule"
+		if device.state == 'Double Click' then
+			-- Lampen uitzetten
+			domoticz.devices('Lampen logeerkamer').switchOff().checkFirst()
 			domoticz.log('Lights logeerkamer turned off')
-		elseif (device.state == 'Click') then
+		elseif device.state == 'Click' then
+			-- Lampen switchen
 			if domoticz.devices('Lampen logeerkamer').state == 'On' then
-			    domoticz.devices('Lampen logeerkamer').switchOff()
+			    domoticz.devices('Lampen logeerkamer').switchOff().checkFirst()
 			    domoticz.log('Lights logeerkamer turned off')    
 			elseif domoticz.devices('Lampen logeerkamer').state == 'Off' then
-			    domoticz.devices('Lampen logeerkamer').switchOn()
+			    domoticz.devices('Lampen logeerkamer').switchOn().checkFirst()
 			    domoticz.log('Lights logeerkamer turned on')
 			end
-		end
+		elseif (device.state == 'Long Click') then
+			-- Lampen aanzetten
+			domoticz.devices('Lampen logeerkamer').switchOn().checkFirst()
+			domoticz.log('Lights logeerkamer turned on')
+		end		
 	end
 }
