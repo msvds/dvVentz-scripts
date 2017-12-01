57601bb99ebf0e77d361b742df6ba3efcbb64ad5
msvds <martijnvanderschoot@gmail.com>
2017-12-01 07:51:05 +0100

    Update voordeurbel.lua

--- voordeurbel.lua
+++ voordeurbel.lua
@@ -3,28 +3,16 @@
 return {
 	active = true, -- set to false to disable this script
 	on = {
-		devices = {131
+		devices = {'Voordeurbel'
 		--,91
 		}
 	},
 	execute = function(domoticz, device)
-		testing_on = false
-		local test_switch = domoticz.devices(91)
-		local Time = require('Time')
-		local lamp_hal_boven = domoticz.devices(151)
-		local schemerlamp_deur = domoticz.devices(97)
-		local lamp_spoelb_keuken = domoticz.devices(36)
-		local schemerlamp_bank = domoticz.devices(16)
-		local lamp_boven_tv = domoticz.devices(13)
-		local lamp_voordeur = domoticz.devices(33)
-		local IsDark = domoticz.devices(78)
-		debug = false			
-		domoticz.helpers.sendnotification(domoticz,'De deurbel gaat','Er staat iemand voor de deur, misschien open doen?',schemerlamp_deur.state,lamp_spoelb_keuken.state,lamp_boven_tv.state,schemerlamp_bank.state,lamp_hal_boven.state,1,2,1)
-		if (IsDark == 'On') then
-			if (lamp_voordeur.state == 'Off') then
-				lamp_voordeur.switchOn()
-				domoticz.log('Voordeurlamp aangezet omdat iemand aanbelt in het donker')
-			end
+		local Time = require('Time')		
+		domoticz.helpers.sendnotification(domoticz,'De deurbel gaat','Er staat iemand voor de deur',domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Lamp boven TV').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,1,2,1)
+		if (domoticz.devices('IsDonker (virt)').state == 'On') then
+			domoticz.devices('Voordeurlamp').switchOn().checkFirst()
+			domoticz.log('Voordeurlamp aangezet omdat iemand aanbelt in het donker')
 		end
 	end
 }
