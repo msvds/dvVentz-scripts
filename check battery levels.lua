c344506a751abccd6b7de220884119dae9fff26d
msvds <martijnvanderschoot@gmail.com>
2017-12-08 08:14:53 +0100

    Update check battery levels.lua

--- check battery levels.lua
+++ check battery levels.lua
@@ -27,7 +27,7 @@ return {
 
 		end)
 
-		if (message) then
+		if (string.len(message) > 5) then
 			domoticz.notify('Low battery warning', message, domoticz.PRIORITY_NORMAL)
 			domoticz.log('Low battery warning: ' .. message, domoticz.LOG_ERROR)
 		end
