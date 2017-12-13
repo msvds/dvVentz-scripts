4d2201de0a2b3682fb69aa3d03b0b2646d678503
msvds <martijnvanderschoot@gmail.com>
2017-12-13 07:27:42 +0100

    Update beweging_woonkamer.lua

--- beweging_woonkamer.lua
+++ beweging_woonkamer.lua
@@ -7,10 +7,10 @@ return {
 	execute = function(domoticz, device)
 		if (domoticz.devices('Beweging woonkamer 1').state == 'On' or domoticz.devices('Beweging woonkamer 2').state == 'On') then
 			domoticz.devices('Beweging woonkamer').switchOn()
-			domoticz.log('Beweging woonkamer')
+			--domoticz.log('Beweging woonkamer')
 		else
 			domoticz.devices('Beweging woonkamer').switchOff()
-			domoticz.log('Stop beweging woonkamer')
+			--domoticz.log('Stop beweging woonkamer')
 		end
 	end
 }
