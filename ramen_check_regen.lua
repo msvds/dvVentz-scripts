9d4b94fd7d68e0c4a895fdb960fa8113f04f4ec4
msvds <martijnvanderschoot@gmail.com>
2017-12-19 21:37:37 +0100

    Update ramen_check_regen.lua

--- ramen_check_regen.lua
+++ ramen_check_regen.lua
@@ -9,7 +9,7 @@ return {
 		if (domoticz.devices['Rain Expected Levels'].levelName ~= 'Droog') then
 			if (domoticz.devices('Dakraam slaapkamer').state == 'Open' and domoticz.devices('Notifications').level ~= 0) then
 			   domoticz.notify('Dakraam open bij regen','Het dakraam in de slaapkamer staat open en het gaat regenen',domoticz.PRIORITY_HIGH)
-			elseif (domoticz.devices('Zolderdakraam achter').state == 'Open' and domoticz.devices('Notifications').level <> 0) then
+			elseif (domoticz.devices('Zolderdakraam achter').state == 'Open' and domoticz.devices('Notifications').level ~= 0) then
 			   domoticz.notify('Dakraam open bij regen','Het dakraam op zolder achter staat open en het gaat regenen',domoticz.PRIORITY_HIGH)
 			end 
 		end
