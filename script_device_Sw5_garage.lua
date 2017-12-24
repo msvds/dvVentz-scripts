852d93261746d4bd4e2650cb8e477f9edae561ea
msvds <martijnvanderschoot@gmail.com>
2017-12-24 19:16:22 +0100

    Update script_device_Sw5_garage.lua

--- script_device_Sw5_garage.lua
+++ script_device_Sw5_garage.lua
@@ -19,8 +19,11 @@ return {
 			domoticz.devices('Status').switchSelector(10) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 			domoticz.helpers.switch_all_lights_off(domoticz)
 			domoticz.helpers.check_doors_and_windows(domoticz)
-			domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
-			domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan weggaan knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
+			--domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
+			--domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan weggaan knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(20)
+			domoticz.log('Toon Scenes gezet op Sleep (20) omdat de gaan slapen knop ingedrukt is')
 			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDAWAY) -- This will  the zone "My Home" to "Armed Away" after the default exit delay
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
