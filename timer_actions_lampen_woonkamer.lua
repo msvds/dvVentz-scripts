d33e19188901d361ee58773d68fae29b03149355
msvds <martijnvanderschoot@gmail.com>
2018-01-02 07:21:32 +0100

    Update timer_actions_lampen_woonkamer.lua

--- timer_actions_lampen_woonkamer.lua
+++ timer_actions_lampen_woonkamer.lua
@@ -10,6 +10,8 @@ return {
 
 		-- woonkamer bij zonsondergang
 		domoticz.groups('Lampen woonkamer').switchOn().checkFirst()
-		domoticz.log('lampen woonkamer aangezet ivm zonsondergang')
+		domoticz.log('lampen woonkamer aangezet ivm zonsondergang', domoticz.LOG_INFO)
+		domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
+		domoticz.log('Lamp spoelbak keuken aangezet ivm zonsondergang', domoticz.LOG_INFO)
 	end
 }
