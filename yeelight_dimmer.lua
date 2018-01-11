44842a60fd7e706286b4c64d376220110f62276a
msvds <martijnvanderschoot@gmail.com>
2018-01-11 12:30:22 +0100

    Create yeelight_dimmer.lua

---
+++ yeelight_dimmer.lua
@@ -0,0 +1,31 @@
+return {
+	active = true,
+	logging = {
+		level = domoticz.LOG_INFO,
+		marker = "TEST"
+	},
+	on = {
+		devices = {
+			'Yeelight Dimmer bank'
+		},
+	},
+	execute = function(domoticz, device)
+		local IP = '192.168.178.36';
+		local PORT = '55443'
+		if(domoticz.devices('White Temp Yeelight bank').state=='Off') then 
+			DomValue = 0;
+			runcommandoff = "sudo echo -ne '{\"id\":1,\"method\":\"set_power\", \"params\":[\"off\", \"smooth\", 500]}\\r\\n' | nc -w1 " ..IP.." " ..PORT.."";;
+			os.execute(runcommandoff);
+		else
+			TempValue = otherdevices_svalues['White Temp Yeelight bank'];   
+			CalcValue = ((TempValue-1) * 48)+1700;
+			DomValue = otherdevices_svalues['Yeelight Dimmer bank']; 
+		end
+		if CalcValue==nil then CalcValue=0 end
+
+		runcommand = "sudo echo -ne '{\"id\":1, \"method\":\"set_scene\", \"params\":[\"ct\", " .. CalcValue .. ", " .. DomValue .. "]}\\r\\n' | nc -w1 " ..IP.." " ..PORT.."";
+		os.execute(runcommand);
+		print(runcommand)
+		print("bright value= "..DomValue);
+	end
+}
