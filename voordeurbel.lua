ce8259c71dbfdcea4bf87fc11cf0236041814445
msvds <martijnvanderschoot@gmail.com>
2018-01-11 11:14:06 +0100

    Update voordeurbel.lua

--- voordeurbel.lua
+++ voordeurbel.lua
@@ -8,9 +8,17 @@ return {
 		}
 	},
 	execute = function(domoticz, device)
-		local Time = require('Time')		
+		local Time = require('Time')
+		domoticz.devices('Xiaomi Gateway Doorbell hal boven').switchSelector(10)
+		local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.3:8084/json.htm?type=command&param=setcolbrightnessvalue&idx=53&hue=236&brightness=1&iswhite=false"'
+		os.execute(sceneCmd)
+		
+		domoticz.devices('Xiaomi Gateway Doorbell eetkamer').switchSelector(10)
+		local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.37:8084/json.htm?type=command&param=setcolbrightnessvalue&idx=54&hue=236&brightness=1&iswhite=false"'
+		os.execute(sceneCmd)
+		
 		domoticz.helpers.sendnotification(domoticz,'De deurbel gaat','Er staat iemand voor de deur')
-		domoticz.helpers.flash_lights(domoticz,domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Lamp boven TV').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,1,2,1)
+		--domoticz.helpers.flash_lights(domoticz,domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Lamp boven TV').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,1,2,1)
 		if (domoticz.devices('IsDonker (virt)').state == 'On') then
 			domoticz.devices('Voordeurlamp').switchOn().checkFirst()
 			domoticz.log('Voordeurlamp aangezet omdat iemand aanbelt in het donker')
