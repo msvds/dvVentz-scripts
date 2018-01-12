3663e2e1ef586fd978efedfb751a266fcb980f9d
msvds <martijnvanderschoot@gmail.com>
2018-01-12 19:04:40 +0100

    Update script_device_Sw3_bed.lua

--- script_device_Sw3_bed.lua
+++ script_device_Sw3_bed.lua
@@ -14,6 +14,19 @@ return {
 			for i=1, alarm.qtyAlarmZones() do
 				alarm.zones(i).disArmZone(domoticz)
 			end
+			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+			if (domoticz.devices('Dimmer bed Martijn').state == 'Off') then
+				domoticz.devices('Dimmer bed Martijn').dimTo(20)
+				--domoticz.devices('Dimmer bed Martijn').switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+			end
+			if (domoticz.devices('Dimmer bed Suzanne').state == 'Off') then
+				domoticz.devices('Dimmer bed Suzanne').dimTo(20)
+				--domoticz.devices('Dimmer bed Suzanne').switchOn()
+				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+			end
+			domoticz.devices('Lamp hal boven').switchOn().checkFirst()
+			domoticz.log('Lights turned on')
 		elseif device.state == 'Click' then
 			-- Alles uit en beveiliging aanzetten bij gaan slapen (sleep)
 			domoticz.devices('Status').switchSelector(30) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
