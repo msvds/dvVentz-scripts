0372f562cab8a674d2ecae212114e7d7407d94f6
msvds <martijnvanderschoot@gmail.com>
2018-01-12 18:54:05 +0100

    Update timer_actions_lampen_buiten_off.lua

--- timer_actions_lampen_buiten_off.lua
+++ timer_actions_lampen_buiten_off.lua
@@ -3,13 +3,13 @@
 return {
 	active = true, -- set to false to disable this script
 	on = {
-		timer = {'at 00:10','at 06:10'}
+		timer = {'at 00:10'}
 	},
 	execute = function(domoticz, device)
 		local Time = require('Time')
 
 		-- woonkamer bij zonsondergang
 		domoticz.groups('Buitenlampen').switchOff().checkFirst()
-		domoticz.log('lampen buiten uitgezet ivm zonsopgang en nacht')
+		domoticz.log('lampen buiten uitgezet ivm nacht')
 	end
 }
