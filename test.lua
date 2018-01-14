76a71d346709e889b55d3d627b2e11b76569b209
msvds <martijnvanderschoot@gmail.com>
2018-01-14 08:44:12 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -10,19 +10,19 @@ return {
 		},
 	},
 	execute = function(domoticz, device)
-		domoticz.devices('Xiaomi Gateway Doorbell eetkamer').switchSelector(10)
-		local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.2:8084/json.htm?type=command&param=setcolbrightnessvalue&idx=54&hue=236&brightness=1&iswhite=false"'
-		os.execute(sceneCmd)
+		--domoticz.devices('Xiaomi Gateway Doorbell eetkamer').switchSelector(10)
+		--local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.2:8084/json.htm?type=command&param=setcolbrightnessvalue&idx=54&hue=236&brightness=1&iswhite=false"'
+		--os.execute(sceneCmd)
 		
 		--domoticz.devices('White Temp Yeelight bank').dimTo(20)
 		--domoticz.devices('Yeelight Dimmer bank').dimTo(50)
 		--domoticz.devices('Yeelight bank').switchOn()
-		--domoticz.log('Huidige currentSetpoint is '.. currentSetpoint)
-		--domoticz.log('Verander currentSetpoint naar 10')
+		domoticz.log('Huidige currentSetpoint is '.. currentSetpoint)
+		domoticz.log('Verander currentSetpoint naar 30')
 		--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
-		--domoticz.devices('Toon Scenes').switchSelector(30)
+		domoticz.devices('Toon Scenes').switchSelector(30)
 		--domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gashaard aangezet is',false,domoticz.helpers.currentSetpoint(domoticz))
-		--domoticz.log('Nieuwe currentSetpoint is '.. currentSetpoint)
+		domoticz.log('Nieuwe currentSetpoint is '.. currentSetpoint)
 		--domoticz.log('Huidige setpoint is '.. currentSetpoint)
 		--domoticz.log('Huidige ActiveState is '.. currentActiveState)
 		--domoticz.helpers.changeToonScene(domoticz,'10','gewoon omdat',false,domoticz.helpers.currentSetpoint(domoticz),domoticz.helpers.currentActiveState(domoticz))
