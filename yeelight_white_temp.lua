3cd7f6034583132dd95d0e68febc340eed684c79
msvds <martijnvanderschoot@gmail.com>
2018-01-20 19:58:13 +0100

    Update yeelight_white_temp.lua

--- yeelight_white_temp.lua
+++ yeelight_white_temp.lua
@@ -2,18 +2,18 @@ return {
 	active = true,
 	on = {
 		devices = {
-			'White Temp Yeelight eetkamer'
+			'White Temp Yeelight eetkamer 1'
 		},
 	},
 	execute = function(domoticz, device)
 		local IP = '192.168.178.36';
 		local PORT = '55443'
-		if(domoticz.devices('White Temp Yeelight eetkamer').state=='Off') then 
+		if(domoticz.devices('White Temp Yeelight eetkamer 1').state=='Off') then 
 			DomValue = 0;
 			runcommandoff = "sudo echo -ne '{\"id\":1,\"method\":\"set_power\", \"params\":[\"off\", \"smooth\", 500]}\\r\\n' | nc -w1 " ..IP.." " ..PORT.."";;
 			os.execute(runcommandoff);
 		else
-			DomValue = otherdevices_svalues['White Temp Yeelight eetkamer'];   
+			DomValue = otherdevices_svalues['White Temp Yeelight eetkamer 1'];   
 			CalcValue = ((DomValue-1) * 48)+1700;
 		end
 		if CalcValue==nil then CalcValue=0 end
