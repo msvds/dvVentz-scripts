8848b18c4562ce9eb90e001a2ec0f5bcfb1897a1
msvds <martijnvanderschoot@gmail.com>
2018-01-21 21:31:38 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -12,7 +12,7 @@ return {
 	execute = function(domoticz, device)
 		if domoticz.devices('Test Switch').state == 'Off' then
 			--domoticz.devices('Xiaomi Gateway Doorbell eetkamer').switchSelector(10)
-			local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.37:8084/json.htm?type=command&param=setcolbrightnessvalue&idx=54&hue=236&brightness=1&iswhite=false"'
+			local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://192.168.178.37:9898/json.htm?type=command&param=setcolbrightnessvalue&idx=54&hue=236&brightness=1&iswhite=false"'
 			os.execute(sceneCmd)
 
 			--domoticz.devices('White Temp Yeelight bank').dimTo(20)
