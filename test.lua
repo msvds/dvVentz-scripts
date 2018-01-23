0ac162f77e607b06d124b5b44cedd90362d338ee
msvds <martijnvanderschoot@gmail.com>
2018-01-23 22:29:38 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -11,10 +11,11 @@ return {
 	},
 	execute = function(domoticz, device)
 		if domoticz.devices('Test Switch').state == 'Off' then
-			for i=1,10 do
-				domoticz.devices('Gateway light eetkamer').switchSelector(20)
-				domoticz.devices('Gateway light eetkamer').switchOff
-			end
+
+			domoticz.devices('Gateway light eetkamer').switchSelector(20)
+			domoticz.devices('Gateway light eetkamer').switchOff
+			domoticz.devices('Gateway light eetkamer').switchSelector(20)
+			domoticz.devices('Gateway light eetkamer').switchOff
 			--domoticz.devices('Xiaomi Gateway Doorbell eetkamer').switchSelector(10)
 			-- set to pink
 			--local sceneCmd = 'curl -s -i -H "Accept: application/json" "http://msvds:venefTWI@msvds.duckdns.org:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=413&hex=ff00ff&brightness=100&iswhite=false"'
