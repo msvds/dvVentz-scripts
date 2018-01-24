b5159e9ecc8f619117af66ed75633f8f7e0731f5
msvds <martijnvanderschoot@gmail.com>
2018-01-24 20:10:59 +0100

    Update script_device_Sw1_woonk_deur.lua

--- script_device_Sw1_woonk_deur.lua
+++ script_device_Sw1_woonk_deur.lua
@@ -18,6 +18,8 @@ return {
 			if IsDark.state == 'On' then
 				domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 				domoticz.devices('Lamp boven TV').switchOn().checkFirst()
+				domoticz.devices('Grote lamp naast bank').switchOn().checkFirst()
+				domoticz.devices('Lamp speelkamer').switchOn().checkFirst()
 				domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
 				domoticz.devices('Lamp ster').switchOn().checkFirst()
 				domoticz.devices('Yeelight eetkamer 1').switchOn().checkFirst()
@@ -41,6 +43,8 @@ return {
 			-- Lampen aanzetten
 			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 			domoticz.devices('Lamp boven TV').switchOn().checkFirst()
+			domoticz.devices('Grote lamp naast bank').switchOn().checkFirst()
+			domoticz.devices('Lamp speelkamer').switchOn().checkFirst()
 			domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
 			domoticz.devices('Lamp ster').switchOn().checkFirst()
 			domoticz.devices('Yeelight eetkamer 1').switchOn().checkFirst()
