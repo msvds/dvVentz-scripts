5f966c19d33cd85f69853eb2f0489121604bbff1
msvds <martijnvanderschoot@gmail.com>
2018-01-24 07:35:50 +0100

    Update alarm_status_indicator.lua

--- alarm_status_indicator.lua
+++ alarm_status_indicator.lua
@@ -10,14 +10,20 @@ return {
 
 		-- Handle the Alarm Status Indicator
 		if (device.id == alarm.zones('My Home').armingModeTextDevID) then
-			if device.state == domoticz.SECURITY_DISARMED then 
+			if device.state == domoticz.SECURITY_DISARMED then				
+				domoticz.devices('Gateway light eetkamer').switchSelector(0)
+				domoticz.devices('Gateway light hal boven').switchSelector(0)
 				domoticz.devices('Xiaomi RGB Gateway eetkamer').switchOff() -- light off
 				domoticz.devices('Xiaomi RGB Gateway hal boven').switchOff() -- light off
 				domoticz.log('SECURITY DISARMED')
-			else
-				--domoticz.devices('Xiaomi RGB Gateway eetkamer').switchOn() -- Red light on
-				--domoticz.devices('Xiaomi RGB Gateway hal boven').switchOn() -- Red light on
-				domoticz.log('SECURITY ARMED')
+			elseif device.state == domoticz.domoticz.SECURITY_ARMEDAWAY then 
+				domoticz.devices('Gateway light eetkamer').switchSelector(10)
+				domoticz.devices('Gateway light hal boven').switchSelector(10)
+				domoticz.log('SECURITY ARMED AWAY')
+			elseif device.state == domoticz.domoticz.SECURITY_ARMEDHOME then 
+				domoticz.devices('Gateway light eetkamer').switchSelector(70)
+				domoticz.devices('Gateway light hal boven').switchSelector(70)
+				domoticz.log('SECURITY ARMED HOME')
 			end
 		end
 
