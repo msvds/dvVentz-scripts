063a2de5257bcc09684c2df9a7d54372e1646fa7
msvds <martijnvanderschoot@gmail.com>
2018-01-26 07:34:01 +0100

    Update script_device_Sw3_bed.lua

--- script_device_Sw3_bed.lua
+++ script_device_Sw3_bed.lua
@@ -37,9 +37,9 @@ return {
 			--domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
 			--domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan slapen knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
 			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDHOME) -- This will  the zone "My Home" to "Armed Home" after the default exit delay
 			domoticz.devices('Toon Scenes').switchSelector(20)
 			domoticz.log('Toon Scenes gezet op Sleep (20) omdat de gaan slapen knop ingedrukt is')
-			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDHOME) -- This will  the zone "My Home" to "Armed Home" after the default exit delay
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
 			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
