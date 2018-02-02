442fda3aa3736c1cd2e0fa2cdb828d797d31ae9c
msvds <martijnvanderschoot@gmail.com>
2018-02-02 09:05:28 +0100

    Update script_device_Sw3_bed.lua

--- script_device_Sw3_bed.lua
+++ script_device_Sw3_bed.lua
@@ -19,14 +19,17 @@ return {
 				if (domoticz.devices('Dimmer bed Martijn').state == 'Off') then
 					domoticz.devices('Dimmer bed Martijn').dimTo(20)
 					--domoticz.devices('Dimmer bed Martijn').switchOn()
-					domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+					--domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
 				end
 				if (domoticz.devices('Dimmer bed Suzanne').state == 'Off') then
 					domoticz.devices('Dimmer bed Suzanne').dimTo(20)
 					--domoticz.devices('Dimmer bed Suzanne').switchOn()
-					domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+					--domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
 				end
 				domoticz.devices('Lamp hal boven').switchOn().checkFirst()
+				domoticz.devices('White Temp Yeelight slaapkamer').dimTo(20)
+				domoticz.devices('Yeelight Dimmer slaapkamer').dimTo(50)
+				domoticz.devices('Yeelight slaapkamer').switchOn().checkFirst()
 				domoticz.log('Lights turned on')
 			end
 		elseif device.state == 'Click' then
@@ -54,6 +57,9 @@ return {
 				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
 			end
 			domoticz.devices('Lamp hal boven').switchOn().checkFirst()
+			domoticz.devices('White Temp Yeelight slaapkamer').dimTo(20)
+			domoticz.devices('Yeelight Dimmer slaapkamer').dimTo(50)
+			domoticz.devices('Yeelight slaapkamer').switchOn().checkFirst()
 			domoticz.log('Lights turned on')
 		end		
 	end
