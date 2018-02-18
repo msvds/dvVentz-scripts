6651efcbc95695d04829c1e7e22c120983880bc3
msvds <martijnvanderschoot@gmail.com>
2018-02-18 09:56:47 +0100

    Update script_device_Sw6_badkamer.lua

--- script_device_Sw6_badkamer.lua
+++ script_device_Sw6_badkamer.lua
@@ -8,6 +8,16 @@ return {
 
 	execute = function(domoticz, device)
 		if device.state == 'Double Click' then
+			-- Beveiliging uitzetten (thuis)
+			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+			for i=1, alarm.qtyAlarmZones() do
+				alarm.zones(i).disArmZone(domoticz)
+			end
+			-- Gateway status resetten
+			domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
+			domoticz.devices('Xiaomi Gateway Alarm Ringtone hal boven').switchSelector(0)
+			domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
+			domoticz.devices('Gateway light hal boven').switchSelector(0)--off
 		elseif device.state == 'Click' then
 			domoticz.devices('Bathroom-Stat').updateSetPoint(21)
 			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
