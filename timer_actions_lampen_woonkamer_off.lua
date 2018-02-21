ba1f823d4f5b6636105e6513b08a74bd541aee3a
msvds <martijnvanderschoot@gmail.com>
2018-02-21 08:24:33 +0100

    Update timer_actions_lampen_woonkamer_off.lua

--- timer_actions_lampen_woonkamer_off.lua
+++ timer_actions_lampen_woonkamer_off.lua
@@ -10,11 +10,11 @@ return {
 		debug = false	
 		local Time = require('Time')
 		if debug == true then domoticz.log('IsDark.state = ' ..IsDark.state) end
-		domoticz.groups('Lampen woonkamer').switchOff().checkFirst()
+		domoticz.groups('Lampen woonkamer').switchOff().checkFirst().afterMin(5)
 		domoticz.log('lampen woonkamer uitgezet ivm zonsopgang')
-		domoticz.devices('Schemerlamp deur').switchOff().checkFirst()
+		domoticz.devices('Schemerlamp deur').switchOff().checkFirst().afterMin(5)
 		domoticz.log('Schemerlamp deur uitgezet ivm zonsopgang', domoticz.LOG_INFO)
-		domoticz.devices('Lamp spoelb keuken').switchOff().checkFirst()
+		domoticz.devices('Lamp spoelb keuken').switchOff().checkFirst().afterMin(5)
 		domoticz.log('Lamp spoelbak keuken uitgezet ivm zonsopgang', domoticz.LOG_INFO)
 	end
 }
