360c324013c8b43fdbaea5e70484857fdd9efa70
msvds <martijnvanderschoot@gmail.com>
2018-02-22 16:23:37 +0100

    Update timer_thermostaat.lua

--- timer_thermostaat.lua
+++ timer_thermostaat.lua
@@ -1,17 +1,55 @@
 return {
-	active = false,
+	active = true,
 	on = {
 		timer = {'every 1 minutes'}
 	},
 	execute = function(domoticz, device)
 		local Time = require('Time')
-		if (domoticz.time.matchesRule('at 16:32-21:33 on mon,tue,wed')) then
-			if domoticz.devices('SomeoneHome').state == 'On' then
-				domoticz.devices('Roomlars-Stat').updateSetPoint(10)
-				--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
-				domoticz.devices('Toon Scenes').switchSelector(40)
-				domoticz.log('Toon Scenes gezet op Sleep (20) omdat')
-			end
+		if (domoticz.time.matchesRule('at 7:30-7:35 on mon,tue,wed,thu,fri') and domoticz.devices('SomeoneHome').state == 'On') then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(30)
+			domoticz.log('Toon Scenes gezet op Home (30) volgens timer programma')
 		end
+		if (domoticz.time.matchesRule('at 8:00-8:05 on mon,tue')) then			
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(10)
+			domoticz.log('Toon Scenes gezet op Away (10) volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 14:00-14:05 on mon') and domoticz.devices('SomeoneHome').state == 'On') then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(30)
+			domoticz.log('Toon Scenes gezet op Home (30) volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 17:00-17:05 on mon,wed,thu,fri') and domoticz.devices('SomeoneHome').state == 'On') then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(30)
+			domoticz.log('Toon Scenes gezet op Home (30) volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 18:10-18:15 on tue') and domoticz.devices('SomeoneHome').state == 'On') then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(30)
+			domoticz.log('Toon Scenes gezet op Home (30) volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 23:55-23:59')) then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(20)
+			domoticz.log('Toon Scenes gezet op Sleep (20) volgens timer programma')
+			domoticz.devices('Roomlars-Stat').updateSetPoint(10)
+			domoticz.devices('Bathroom-Stat').updateSetPoint(10)
+			domoticz.devices('Chillroom-Stat').updateSetPoint(10)
+			domoticz.devices('Roomnienke-Stat').updateSetPoint(10)
+			domoticz.log('Alle thermostaatkranen teruggezet op 10 graden volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 21:15-21:20 on mon,tue,wed,thu,fri')) then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(20)
+			domoticz.log('Toon Scenes gezet op Sleep (20) volgens timer programma')
+		end
+		if (domoticz.time.matchesRule('at 22:15-22:20 on sat,sun')) then				
+			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
+			domoticz.devices('Toon Scenes').switchSelector(20)
+			domoticz.log('Toon Scenes gezet op Sleep (20) volgens timer programma')
+		end
+		
 	end
 }
