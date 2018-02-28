aa77d6eadcbb03cb63dfe086ff20359afea370f1
msvds <martijnvanderschoot@gmail.com>
2018-02-28 08:14:00 +0100

    Update timer_actions_lampen_woonkamer_off.lua

--- timer_actions_lampen_woonkamer_off.lua
+++ timer_actions_lampen_woonkamer_off.lua
@@ -1,5 +1,5 @@
 return {
-	active = false, -- set to false to disable this script
+	active = true, -- set to false to disable this script
 	on = {
 		devices = {
 			'IsDonker (virt)'
@@ -10,11 +10,13 @@ return {
 		debug = false	
 		local Time = require('Time')
 		if debug == true then domoticz.log('IsDark.state = ' ..IsDark.state) end
-		domoticz.groups('Lampen woonkamer').switchOff().checkFirst().afterMin(10)
-		domoticz.log('lampen woonkamer uitgezet ivm zonsopgang')
-		domoticz.devices('Schemerlamp deur').switchOff().checkFirst().afterMin(10)
-		domoticz.log('Schemerlamp deur uitgezet ivm zonsopgang', domoticz.LOG_INFO)
-		domoticz.devices('Lamp spoelb keuken').switchOff().checkFirst().afterMin(10)
-		domoticz.log('Lamp spoelbak keuken uitgezet ivm zonsopgang', domoticz.LOG_INFO)
+		if (domoticz.devices('IsDonker (virt)').state == 'Off') then
+			domoticz.groups('Lampen woonkamer').switchOff().checkFirst().afterMin(10)
+			domoticz.log('lampen woonkamer uitgezet ivm zonsopgang')
+			domoticz.devices('Schemerlamp deur').switchOff().checkFirst().afterMin(10)
+			domoticz.log('Schemerlamp deur uitgezet ivm zonsopgang', domoticz.LOG_INFO)
+			domoticz.devices('Lamp spoelb keuken').switchOff().checkFirst().afterMin(10)
+			domoticz.log('Lamp spoelbak keuken uitgezet ivm zonsopgang', domoticz.LOG_INFO)
+		end
 	end
 }
