504b63680a86420813edec9db9a068d40c8dfd4f
msvds <martijnvanderschoot@gmail.com>
2018-03-14 19:31:34 +0100

    Update script_device_Switch_Slaapkamer.lua

--- script_device_Switch_Slaapkamer.lua
+++ script_device_Switch_Slaapkamer.lua
@@ -10,7 +10,6 @@ return {
 		local alarm = require "ideAlarmModule"
 		if device.state == 'Switch 1' then
 			-- Lampen aanzetten
-			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 			if (domoticz.devices('Dimmer bed Martijn').state == 'Off') then
 				domoticz.devices('Dimmer bed Martijn').dimTo(20)
 				--domoticz.devices('Dimmer bed Martijn').switchOn()
@@ -20,14 +19,20 @@ return {
 				domoticz.devices('Dimmer bed Suzanne').dimTo(20)
 				--domoticz.devices('Dimmer bed Suzanne').switchOn()
 				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
-			end
-			domoticz.devices('Lamp hal boven').switchOn().checkFirst()
+			end		
+			domoticz.log('Nachtlampjes aangezet')
+		elseif device.state == 'Switch 2' then
+			-- Lampen aanzetten
 			domoticz.devices('White Temp Yeelight slaapkamer').dimTo(20)
 			domoticz.devices('Yeelight Dimmer slaapkamer').dimTo(50)
 			domoticz.devices('Yeelight slaapkamer').switchOn().checkFirst()
-			domoticz.log('Lights turned on')
-		elseif device.state == 'Switch 2' then
+			domoticz.log('Lamp slaapkamer aangezet')
 		elseif (device.state == 'Both_Click') then
+			-- Lampen uitzetten
+			domoticz.devices('Dimmer bed Martijn').switchOff()
+			domoticz.devices('Dimmer bed Suzanne').switchOff()
+			domoticz.devices('Yeelight slaapkamer').switchOff().checkFirst()
+			domoticz.log('Alle lampen slaapkamer uitgezet')
 		end		
 	end
 }
