1cc992afc314b421b601ae72d1643fff2a315dfb
msvds <martijnvanderschoot@gmail.com>
2018-03-14 20:11:40 +0100

    Update script_device_Switch_Slaapkamer.lua

--- script_device_Switch_Slaapkamer.lua
+++ script_device_Switch_Slaapkamer.lua
@@ -8,25 +8,32 @@ return {
 
 	execute = function(domoticz, device)
 		local alarm = require "ideAlarmModule"
-		if device.state == 'Switch 1' then
-			-- Lampen aanzetten
+		if device.state == 'Switch 1' then		
 			if (domoticz.devices('Dimmer bed Martijn').state == 'Off') then
 				domoticz.devices('Dimmer bed Martijn').dimTo(20)
-				--domoticz.devices('Dimmer bed Martijn').switchOn()
-				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+				domoticz.log('Switch 1 ingedrukt, Nachtlampje Martijn aangezet', domoticz.LOG_INFO)
+			elseif domoticz.devices('Dimmer bed Martijn').state == 'On then	
+				domoticz.devices('Dimmer bed Martijn').switchOff()
+				domoticz.log('Switch 1 ingedrukt, Nachtlampje Martijn uitgezet', domoticz.LOG_INFO)
 			end
 			if (domoticz.devices('Dimmer bed Suzanne').state == 'Off') then
 				domoticz.devices('Dimmer bed Suzanne').dimTo(20)
-				--domoticz.devices('Dimmer bed Suzanne').switchOn()
-				domoticz.log('Slaapkamerdeur open terwijl het donker is, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+				domoticz.log('Switch 1 ingedrukt, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
+			elseif (domoticz.devices('Dimmer bed Suzanne').state == 'On') then
+				domoticz.devices('Dimmer bed Suzanne').switchOff()
+				domoticz.log('Switch 1 ingedrukt, Nachtlampje Suzanne aangezet', domoticz.LOG_INFO)
 			end		
 			domoticz.log('Nachtlampjes aangezet')
 		elseif device.state == 'Switch 2' then
-			-- Lampen aanzetten
-			domoticz.devices('White Temp Yeelight slaapkamer').dimTo(20)
-			domoticz.devices('Yeelight Dimmer slaapkamer').dimTo(50)
-			domoticz.devices('Yeelight slaapkamer').switchOn().checkFirst()
-			domoticz.log('Lamp slaapkamer aangezet')
+			if (domoticz.devices('Yeelight slaapkamer').state == 'Off') then
+				domoticz.devices('White Temp Yeelight slaapkamer').dimTo(20)
+				domoticz.devices('Yeelight Dimmer slaapkamer').dimTo(50)
+				domoticz.devices('Yeelight slaapkamer').switchOn().checkFirst()
+				domoticz.log('Lamp slaapkamer aangezet')
+			elseif (domoticz.devices('Yeelight slaapkamer').state == 'On') then
+				domoticz.devices('Yeelight slaapkamer').switchOff().checkFirst()
+				domoticz.log('Lamp slaapkamer aangezet')
+			end
 		elseif (device.state == 'Both_Click') then
 			-- Lampen uitzetten
 			domoticz.devices('Dimmer bed Martijn').switchOff()
