12b6fd82aed4f93f289a62db580c60d13ea3c9ee
msvds <martijnvanderschoot@gmail.com>
2018-08-09 10:10:45 +0200

    Update kamerplanten.lua

--- kamerplanten.lua
+++ kamerplanten.lua
@@ -26,13 +26,13 @@ return {
 		end
 		
 		if (domoticz.devices('plant moisture 1').percentage < 25) then				
-        	message = message .. 'De plant in de hal boven begint heel erg droog te worden (<25%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
+        		message = message .. 'De plant in de hal boven begint heel erg droog te worden (<25%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
 		elseif (domoticz.devices('plant moisture 1').percentage < 35) then				
-        	message = message .. 'De plant in de hal boven begint erg droog te worden (<35%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
+        		message = message .. 'De plant in de hal boven begint erg droog te worden (<35%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
 		elseif (domoticz.devices('plant moisture 1').percentage < 40) then				
-        	message = message .. 'De plant in de hal boven begint droog te worden (<40%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
+        		message = message .. 'De plant in de hal boven begint droog te worden (<40%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
 		elseif (domoticz.devices('plant moisture 1').percentage < 50) then				
-        	message = message .. 'De plant in de hal boven begint een beetje droog te worden (<50%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
+        		message = message .. 'De plant in de hal boven begint een beetje droog te worden (<50%), namelijk ' ..tonumber(domoticz.devices('plant moisture 1').percentage) .. '), '
 		end
     
 		if (domoticz.devices('plant moisture 2').percentage < 5) then				
@@ -61,9 +61,10 @@ return {
 		--	message = message .. 'De plant in de badkamer drinkt redelijk weinig, klopt het dat het winter of herfst is?, '
 		--end
 		
-		domoticz.devices('Status Notifications').updateText(message).silent()
+		
 		if (string.len(message) > 5 and domoticz.devices('Notifications').level == 20) then
 			domoticz.notify('Planten update', message, domoticz.PRIORITY_LOW)
+			domoticz.devices('Status Notifications').updateText(message).silent()
 		end		
 	end
 }
