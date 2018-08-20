5a0c8d0432136b7e23a7fb2ed34e29b07ffa2d61
msvds <martijnvanderschoot@gmail.com>
2018-08-20 09:27:05 +0200

    Update script_device_Sw6_badkamer.lua

--- script_device_Sw6_badkamer.lua
+++ script_device_Sw6_badkamer.lua
@@ -18,11 +18,31 @@ return {
 			domoticz.devices('Xiaomi Gateway Alarm Ringtone hal boven').switchSelector(0)
 			domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
 			domoticz.devices('Gateway light hal boven').switchSelector(0)--off
+			if IsDark.state == 'On' then
+				domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+				domoticz.groups('Lampen woonkamer').switchOn().checkFirst()
+				domoticz.devices('Schemerlamp deur').switchOn().checkFirst()
+				domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
+				domoticz.log('Lights turned on')
+			end
 		elseif device.state == 'Click' then
-			domoticz.devices('Bathroom-Stat').updateSetPoint(21).checkFirst().formin(60)
+			-- Alles uit en beveiliging aanzetten bij gaan weggaan (away)
+			domoticz.devices('Status').switchSelector(10) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+			domoticz.helpers.switch_all_lights_off(domoticz)
+			domoticz.helpers.check_doors_and_windows(domoticz)
+			--domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
+			--domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan weggaan knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
 			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
-			domoticz.devices('Toon Scenes').switchSelector(40).checkFirst().formin(60)
+			domoticz.devices('Toon Scenes').switchSelector(20)
+			domoticz.log('Toon Scenes gezet op Sleep (20) omdat de gaan slapen knop ingedrukt is')
+			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDAWAY) -- This will  the zone "My Home" to "Armed Away" after the default exit delay
 		elseif (device.state == 'Long Click') then
+			-- Lampen aanzetten
+			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+			domoticz.groups('Lampen woonkamer').switchOn().checkFirst()
+			domoticz.devices('Schemerlamp deur').switchOn().checkFirst()
+			domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
+			domoticz.log('Lights turned on')
 		end		
 	end
 }
