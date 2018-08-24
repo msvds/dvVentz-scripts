107480d6e34ea4cac86308a5c65447c36af2a302
msvds <martijnvanderschoot@gmail.com>
2018-08-24 15:34:13 +0200

    Update script_device_Sw1_woonk_deur.lua

--- script_device_Sw1_woonk_deur.lua
+++ script_device_Sw1_woonk_deur.lua
@@ -22,16 +22,7 @@ return {
 			-- Lampen aanzetten
 			if IsDark.state == 'On' then
 				domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
-				domoticz.devices('Lamp boven TV').switchOn().checkFirst()
-				domoticz.devices('Grote lamp naast bank').switchOn().checkFirst()
-				domoticz.devices('Lamp speelkamer').switchOn().checkFirst()
-				domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
-				domoticz.devices('Lamp ster').switchOn().checkFirst()
-				domoticz.devices('Yeelight eetkamer 1').switchOn().checkFirst()
-				domoticz.devices('Yeelight eetkamer 2').switchOn().checkFirst()
-				domoticz.devices('Schemerlamp bank').switchOn().checkFirst()
-				domoticz.devices('Schemerlamp deur').switchOn().checkFirst()
-				domoticz.log('Lights turned on')
+				domoticz.helpers.switch_lights(domoticz,'Floor1','On')
 			end
 		elseif device.state == 'Click' then
 			-- Alles uit bij gaan slapen (sleep)
@@ -47,16 +38,7 @@ return {
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
 			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
-			domoticz.devices('Lamp boven TV').switchOn().checkFirst()
-			domoticz.devices('Grote lamp naast bank').switchOn().checkFirst()
-			domoticz.devices('Lamp speelkamer').switchOn().checkFirst()
-			domoticz.devices('Lamp spoelb keuken').switchOn().checkFirst()
-			domoticz.devices('Lamp ster').switchOn().checkFirst()
-			domoticz.devices('Yeelight eetkamer 1').switchOn().checkFirst()
-			domoticz.devices('Yeelight eetkamer 2').switchOn().checkFirst()
-			domoticz.devices('Schemerlamp bank').switchOn().checkFirst()
-			domoticz.devices('Schemerlamp deur').switchOn().checkFirst()
-			domoticz.log('Lights turned on')
+			domoticz.helpers.switch_lights(domoticz,'Floor1','On')
 		end		
 	end
 }
