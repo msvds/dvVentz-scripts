af7bd9e4f0fe0555d257d9bd2acb88e6c698e038
msvds <martijnvanderschoot@gmail.com>
2018-08-29 14:28:20 +0200

    Update gashaard.lua

--- gashaard.lua
+++ gashaard.lua
@@ -8,7 +8,7 @@ return {
 		if (device.state == 'Run Up') then			
 			--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
 			domoticz.devices('Toon Scenes').switchSelector(10)
-			domoticz.log('Toon Scenes gezet op Away (10) omdat de gashaard aangezet is')
+			domoticz.log('Toon Scenes gezet op Away (10) omdat de gashaard aangezet is',domoticz.LOG_INFO)
 		end
 	end
 }
