0781a3a4f1ed868b9abcd4c264a268c22babb9c5
msvds <martijnvanderschoot@gmail.com>
2018-08-29 14:25:45 +0200

    Update script_device_Switch_Lamp_Lars.lua

--- script_device_Switch_Lamp_Lars.lua
+++ script_device_Switch_Lamp_Lars.lua
@@ -11,10 +11,10 @@ return {
 		elseif device.state == 'Click' then
 			if domoticz.devices('Single Wall Switch Lamp Lars').state == 'On' then
 				domoticz.devices('Single Wall Switch Lamp Lars').switchOff()
-				domoticz.log('Lampen kamer Lars uitgezet')
+				domoticz.log('Lampen kamer Lars uitgezet',domoticz.LOG_INFO)
 			elseif domoticz.devices('Single Wall Switch Lamp Lars').state == 'Off' then
 				domoticz.devices('Single Wall Switch Lamp Lars').switchOn()	
-				domoticz.log('Lampen kamer Lars aangezet')
+				domoticz.log('Lampen kamer Lars aangezet',domoticz.LOG_INFO)
 			end
 		elseif (device.state == 'Long Click') then
 		end	
