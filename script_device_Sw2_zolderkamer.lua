1e1fd57abb75457917d367a5e32fc67100e1088b
msvds <martijnvanderschoot@gmail.com>
2018-08-29 14:21:41 +0200

    Update script_device_Sw2_zolderkamer.lua

--- script_device_Sw2_zolderkamer.lua
+++ script_device_Sw2_zolderkamer.lua
@@ -11,20 +11,20 @@ return {
 		if device.state == 'Double Click' then
 			-- Lampen uitzetten
 			domoticz.devices('Lampen logeerkamer').switchOff().checkFirst()
-			domoticz.log('Lights logeerkamer turned off')
+			domoticz.log('Lights logeerkamer turned off',domoticz.LOG_INFO)
 		elseif device.state == 'Click' then
 			-- Lampen switchen
 			if domoticz.devices('Lampen logeerkamer').state == 'On' then
 			    domoticz.devices('Lampen logeerkamer').switchOff().checkFirst()
-			    domoticz.log('Lights logeerkamer turned off')    
+			    domoticz.log('Lights logeerkamer turned off',domoticz.LOG_INFO)   
 			elseif domoticz.devices('Lampen logeerkamer').state == 'Off' then
 			    domoticz.devices('Lampen logeerkamer').switchOn().checkFirst()
-			    domoticz.log('Lights logeerkamer turned on')
+			    domoticz.log('Lights logeerkamer turned on',domoticz.LOG_INFO)
 			end
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
 			domoticz.devices('Lampen logeerkamer').switchOn().checkFirst()
-			domoticz.log('Lights logeerkamer turned on')
+			domoticz.log('Lights logeerkamer turned on',domoticz.LOG_INFO)
 		end		
 	end
 }
