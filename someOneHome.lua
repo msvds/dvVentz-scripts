34cf67d27eb13f4128b6e408ccc8927db79f1382
msvds <martijnvanderschoot@gmail.com>
2018-08-29 14:24:05 +0200

    Update someOneHome.lua

--- someOneHome.lua
+++ someOneHome.lua
@@ -14,11 +14,11 @@ return {
 		local open_delay = 0
 		--if (domoticz.devices('SomeoneHome').state == 'Off' and (domoticz.devices('Suzanne smartphone').state == 'On' or domoticz.devices('Martijn smartphone').state == 'On') or domoticz.globalData.MC_Overall > movement_delay or domoticz.globalData.OpenC_Overall > open_delay) then
 		if (domoticz.devices('SomeoneHome').state == 'Off' and (domoticz.devices('Life360 - Suzanne Presence').state == 'On' or domoticz.devices('Life360 - Martijn Presence').state == 'On')) then
-			domoticz.log('SomeoneHome switchOn')
+			domoticz.log('SomeoneHome switchOn',domoticz.LOG_INFO)
 			domoticz.devices('SomeoneHome').switchOn()
 		--elseif (domoticz.devices('SomeoneHome').state == 'On' and (domoticz.devices('Suzanne smartphone').state == 'Off' and domoticz.devices('Martijn smartphone').state == 'Off') and domoticz.globalData.NMC_Overall > nomovement_delay) then
 		elseif (domoticz.devices('SomeoneHome').state == 'On' and (domoticz.devices('Life360 - Suzanne Presence').state == 'Off' and domoticz.devices('Life360 - Martijn Presence').state == 'Off')) then
-			domoticz.log('SomeoneHome switchOff')
+			domoticz.log('SomeoneHome switchOff',domoticz.LOG_INFO)
 			domoticz.devices('SomeoneHome').switchOff()
 		end
 	end
