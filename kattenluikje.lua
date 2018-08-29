22f8c4ed690a8d72541f5cfb71bf27d3e5c700ef
msvds <martijnvanderschoot@gmail.com>
2018-08-29 14:20:44 +0200

    Update kattenluikje.lua

--- kattenluikje.lua
+++ kattenluikje.lua
@@ -12,16 +12,16 @@ return {
 		if (domoticz.time.matchesRule('at 9:59-10:01')) then
 			if (domoticz.data.counter == 0) then
 				domoticz.helpers.sendnotification(domoticz,'Kattenluikje','De laatste tijd is het kattenluikje niet gebruikt, check even of alles goed werkt')
-				domoticz.log('De laatste tijd is het kattenluikje niet gebruikt, check even of alles goed werkt')
+				domoticz.log('De laatste tijd is het kattenluikje niet gebruikt, check even of alles goed werkt',domoticz.LOG_INFO)
 			else
 				domoticz.helpers.sendnotification(domoticz,'Kattenluikje','Het kattenluikje is de afgelopen 24 uur ' ..domoticz.data.counter .. 'x gebruikt.')
-				domoticz.log('Het kattenluikje is de afgelopen 24 uur ' ..domoticz.data.counter .. 'x gebruikt.')
+				domoticz.log('Het kattenluikje is de afgelopen 24 uur ' ..domoticz.data.counter .. 'x gebruikt.',domoticz.LOG_INFO)
 			end
 			domoticz.data.counter = 0 -- reset the counter
 		else
 			if (domoticz.devices('Kattenluikje').lastUpdate.minutesAgo > 2) then
 				domoticz.data.counter = domoticz.data.counter + 1
-				domoticz.log('De laatste tijd is het kattenluikje niet gebruikt, check even of alles goed werkt')
+				domoticz.log('De laatste tijd is het kattenluikje niet gebruikt, check even of alles goed werkt',domoticz.LOG_INFO)
 			end
 		end
 	end
