3d9b4f96faae203a0c621ec5306925d8e675dd3a
msvds <martijnvanderschoot@gmail.com>
2018-08-31 13:09:01 +0200

    Update script_device_Sw5_garage.lua

--- script_device_Sw5_garage.lua
+++ script_device_Sw5_garage.lua
@@ -22,13 +22,14 @@ return {
 			-- Lampen aanzetten
 			if IsDark.state == 'On' then
 				domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
-				domoticz.groups('Buitenlampen').switchOn().checkFirst()
+				domoticz.helpers.switch_lights(domoticz,'Outside','On')
 				domoticz.log('Outside Lights turned on',domoticz.LOG_INFO)
 			end
 		elseif device.state == 'Click' then
 			-- Alles uit en beveiliging aanzetten bij gaan weggaan (away)
 			domoticz.devices('Status').switchSelector(10) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
 			domoticz.helpers.switch_lights(domoticz,'Inside','Off')
+			domoticz.helpers.switch_lights(domoticz,'Outside','Off')
 			domoticz.helpers.check_doors_and_windows(domoticz)
 			--domoticz.log('Huidige setpoint is '.. domoticz.helpers.currentSetpoint(domoticz))
 			--domoticz.helpers.changeSetPoint(domoticz,'10','omdat de gaan weggaan knop ingedrukt is',false,domoticz.helpers.currentSetpoint(domoticz))
@@ -39,7 +40,7 @@ return {
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
 			domoticz.devices('Status').switchSelector(40) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
-			domoticz.groups('Buitenlampen').switchOn().checkFirst()
+			domoticz.helpers.switch_lights(domoticz,'Outside','On')
 			domoticz.log('Outside Lights turned on',domoticz.LOG_INFO)
 		end		
 	end
