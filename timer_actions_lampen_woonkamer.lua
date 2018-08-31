f4240eae984038c00b6eb2a2589e9f7481c7dd3d
msvds <martijnvanderschoot@gmail.com>
2018-08-31 13:04:13 +0200

    Update timer_actions_lampen_woonkamer.lua

--- timer_actions_lampen_woonkamer.lua
+++ timer_actions_lampen_woonkamer.lua
@@ -9,9 +9,7 @@ return {
 		local Time = require('Time')
 
 		-- woonkamer bij zonsondergang
-		domoticz.helpers.switch_lights(domoticz,'Woonkamer','Off')
-		domoticz.log('lampen woonkamer aangezet ivm zonsondergang', domoticz.LOG_INFO)
 		domoticz.helpers.switch_lights(domoticz,'Woonkamer','On')
-		domoticz.log('Lamp spoelbak keuken aangezet ivm zonsondergang', domoticz.LOG_INFO)
+		domoticz.log('lampen woonkamer aangezet ivm zonsondergang', domoticz.LOG_INFO)
 	end
 }
