d4c51168fd71c835a28b376c2f1fd67ccf0172e6
msvds <martijnvanderschoot@gmail.com>
2018-08-31 12:59:37 +0200

    Update script_device_Switch_Eetkamer.lua

--- script_device_Switch_Eetkamer.lua
+++ script_device_Switch_Eetkamer.lua
@@ -7,21 +7,11 @@ return {
 	},
 
 	execute = function(domoticz, device)
-		if (domoticz.devices('Yeelight eetkamer 1').state == 'On') then
-			domoticz.devices('Yeelight eetkamer 1').switchOff().checkFirst()	
+		if (domoticz.devices('Yeelight eetkamer 1').state == 'On' or omoticz.devices('Yeelight eetkamer 2').state == 'On') then
+			domoticz.helpers.switch_lights(domoticz,'Eetkamer','Off')	
 		end
-		if (domoticz.devices('Yeelight eetkamer 1').state == 'Off' ) then
-			domoticz.devices('White Temp Yeelight eetkamer 1').dimTo(20)
-			domoticz.devices('Yeelight Dimmer eetkamer 1').dimTo(50)
-			domoticz.devices('Yeelight eetkamer 1').switchOn().checkFirst()	
-		end
-		if (domoticz.devices('Yeelight eetkamer 2').state == 'On' ) then
-			domoticz.devices('Yeelight eetkamer 2').switchOff().checkFirst()
-		end
-		if (domoticz.devices('Yeelight eetkamer 2').state == 'Off' ) then
-			domoticz.devices('White Temp Yeelight eetkamer 2').dimTo(20)
-			domoticz.devices('Yeelight Dimmer eetkamer 2').dimTo(50)
-			domoticz.devices('Yeelight eetkamer 2').switchOn().checkFirst()
+		if (domoticz.devices('Yeelight eetkamer 1').state == 'Off' or omoticz.devices('Yeelight eetkamer 2').state == 'Off') then
+			domoticz.helpers.switch_lights(domoticz,'Eetkamer','On')
 		end
 	end
 }
