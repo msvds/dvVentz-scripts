d1d2ffa3ba29c1ac84c4a2ce8ca81f18afc15999
msvds <martijnvanderschoot@gmail.com>
2018-09-01 14:47:46 +0200

    Update script_device_Switch_Hal_Beneden_Voordeurlamp.lua

--- script_device_Switch_Hal_Beneden_Voordeurlamp.lua
+++ script_device_Switch_Hal_Beneden_Voordeurlamp.lua
@@ -8,10 +8,10 @@ return {
 
 	execute = function(domoticz, device)
 		if domoticz.devices('Voordeurlamp').state == 'On' then
-			domoticz.devices('Voordeurlamp').switchOff().checkFirst()
+			domoticz.devices('Voordeurlamp').switchOff()
 			domoticz.log('Voordeurlamp uitgezet',domoticz.LOG_INFO)
 		elseif domoticz.devices('Voordeurlamp').state == 'Off' then
-			domoticz.devices('Voordeurlamp').switchOn()	.checkFirst()
+			domoticz.devices('Voordeurlamp').switchOn()
 			domoticz.log('Voordeurlamp aangezet',domoticz.LOG_INFO)
 		end	
 	end
