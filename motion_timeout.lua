93f0dce0a5b4711b26bce7d6b13831531eb71cf3
msvds <martijnvanderschoot@gmail.com>
2018-09-06 14:17:17 +0200

    Update motion_timeout.lua

--- motion_timeout.lua
+++ motion_timeout.lua
@@ -25,19 +25,19 @@ return {
 			-- Gateway status resetten
 			domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
 			domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
-			domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
+			--domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
 		end		
 		if (domoticz.globalData.NMC_Floor2 > NM_timeout_floor2) then
 			domoticz.helpers.switch_lights(domoticz,'Floor2','Off')
 			-- Gateway status resetten
 			domoticz.devices('Xiaomi Gateway Alarm Ringtone hal boven').switchSelector(0)
 			domoticz.devices('Gateway light hal boven').switchSelector(0)--off
-			domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
+			--domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
 		end
 		if (domoticz.globalData.NMC_PIR_kamerLars > NM_timeout_kamerLars) then
 			if (domoticz.devices('Roomlars-Stat').SetPoint ~= '10') then
 				domoticz.devices('Roomlars-Stat').updateSetPoint(10)
-				domoticz.log('No movement kamer Lars timeout is reached -> verwarming Lars naar 10 graden gezet',domoticz.LOG_INFO)
+				--domoticz.log('No movement kamer Lars timeout is reached -> verwarming Lars naar 10 graden gezet',domoticz.LOG_INFO)
 				--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
 				--domoticz.devices('Toon Scenes').switchSelector(30)
 				--domoticz.log('Toon Scenes teruggezet op Home (30) door beweging timeout in kamer Lars de verwarming daar is uitgezet')
