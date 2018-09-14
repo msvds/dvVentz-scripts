c174d556a50642efbd3048be5c194a3a46c528c9
msvds <martijnvanderschoot@gmail.com>
2018-09-14 21:03:37 +0200

    Update Switch_lights_halbeneden.lua

--- Switch_lights_halbeneden.lua
+++ Switch_lights_halbeneden.lua
@@ -7,9 +7,9 @@ return {
 
 	execute = function(domoticz, device)
 		if (domoticz.devices('Lampen HalBeneden').state == 'On') then
-			domoticz.helpers.switch_lights(domoticz,'HalBeneden','Off')
-		else
 			domoticz.helpers.switch_lights(domoticz,'HalBeneden','On')
+		else
+			domoticz.helpers.switch_lights(domoticz,'HalBeneden','Off')
 		end
 	end
 }
