9233f7dff69caf2bac2ce647677b4a0c224ca240
msvds <martijnvanderschoot@gmail.com>
2018-09-19 07:55:49 +0200

    Update motion_actions.lua

--- motion_actions.lua
+++ motion_actions.lua
@@ -2,7 +2,7 @@ return {
 	active = true, -- set to false to disable this script
 	logging = {marker = "motion_actions"},
 	on = {
-		devices = {'Eetkamerdeur','Dakraam slaapkamer','Balkondeur slaapkamer','Front door','Balkondeur Nienke','Slaapkamerdeur','Deur bijkeuken','Zolderdakraam achter','Garage deur','Beweging woonkamer','Beweging kamer Lars','Beweging hal boven','Dimmer bed Martijn','Dimmer bed Suzanne','Lamp hal boven','Schemerlamp deur','Lamp spoelb keuken'},
+		devices = {'Eetkamerdeur','Dakraam slaapkamer','Balkondeur slaapkamer','Front door','Balkondeur Nienke','Slaapkamerdeur','Deur bijkeuken','Zolderdakraam achter','Garage deur','Beweging woonkamer','Beweging kamer Lars','Beweging hal boven'},
 	},
 
 	execute = function(domoticz, device)
