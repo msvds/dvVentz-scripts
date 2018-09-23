9e792f77dfe024f7da165f31329b6c9cfbae014e
msvds <martijnvanderschoot@gmail.com>
2018-09-23 10:30:44 +0200

    Update script_device_Sw2_zolderkamer.lua

--- script_device_Sw2_zolderkamer.lua
+++ script_device_Sw2_zolderkamer.lua
@@ -11,20 +11,20 @@ return {
 		local alarm = require "ideAlarmModule"
 		if device.state == 'Double Click' then
 			-- Lampen uitzetten
-			domoticz.helpers.switch_lights(domoticz,'Logeerkamer','Off',3)
+			domoticz.helpers.switch_lights(domoticz,'Logeerkamer','Off',0)
 			domoticz.log('Lights logeerkamer turned off',domoticz.LOG_INFO)
 		elseif device.state == 'Click' then
 			-- Lampen switchen
 			if domoticz.devices('Lampen logeerkamer').state == 'On' then
-			    domoticz.helpers.switch_lights(domoticz,'Logeerkamer','Off',3)
+			    domoticz.helpers.switch_lights(domoticz,'Logeerkamer','Off',0)
 			    domoticz.log('Lights logeerkamer turned off',domoticz.LOG_INFO)   
 			elseif domoticz.devices('Lampen logeerkamer').state == 'Off' then
-			    domoticz.helpers.switch_lights(domoticz,'Logeerkamer','On',3)
+			    domoticz.helpers.switch_lights(domoticz,'Logeerkamer','On',0)
 			    domoticz.log('Lights logeerkamer turned on',domoticz.LOG_INFO)
 			end
 		elseif (device.state == 'Long Click') then
 			-- Lampen aanzetten
-			domoticz.helpers.switch_lights(domoticz,'Logeerkamer','On',3)
+			domoticz.helpers.switch_lights(domoticz,'Logeerkamer','On',0)
 			domoticz.log('Lights logeerkamer turned on',domoticz.LOG_INFO)
 		end		
 	end
