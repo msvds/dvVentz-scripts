dd302fc64ac21f7bf1191e7e9dc9243861c9b561
msvds <martijnvanderschoot@gmail.com>
2018-09-23 09:35:16 +0200

    Update Switch_lights_living.lua

--- Switch_lights_living.lua
+++ Switch_lights_living.lua
@@ -6,15 +6,10 @@ return {
 	},
 
 	execute = function(domoticz, device)
-		domoticz.log(domoticz.devices('Lampen Living').state)
 		if (domoticz.devices('Lampen Living').state == 'Off') then
-			domoticz.log('1a')
 			domoticz.helpers.switch_lights(domoticz,'Living','On')
-			domoticz.log('1b')
 		else
-			domoticz.log('2a')
 			domoticz.helpers.switch_lights(domoticz,'Living','Off')
-			domoticz.log('2b')
 		end
 	end
 }
