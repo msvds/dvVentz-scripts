bb3ecc2a68ed36b695a20e813a6d47a8a84d8937
msvds <martijnvanderschoot@gmail.com>
2018-09-25 09:03:03 +0200

    Update timer_sunrise2.lua

--- timer_sunrise2.lua
+++ timer_sunrise2.lua
@@ -5,8 +5,8 @@ return {
 		timer = {'every 1 minute between 10 minutes after sunrise and 15 minutes after sunrise'}
 	},
 	execute = function(domoticz, device)
-		domoticz.helpers.switch_lights(domoticz,'Floor1','Off',3)
-		domoticz.log('lampen woonkamer uitgezet ivm zonsopgang',domoticz.LOG_INFO)
+		domoticz.helpers.switch_lights(domoticz,'Inside','Off',3)
+		domoticz.log('lampen binnen uitgezet ivm zonsopgang',domoticz.LOG_INFO)
 		-- Gateway status resetten
 		domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
 		domoticz.devices('Xiaomi Gateway Alarm Ringtone hal boven').switchSelector(0)
