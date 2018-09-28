eca11f9887f52253cbc631a1ed5a3e69dc744076
msvds <martijnvanderschoot@gmail.com>
2018-09-28 09:58:03 +0200

    Update timer_actions_lampen_kamerlars.lua

--- timer_actions_lampen_kamerlars.lua
+++ timer_actions_lampen_kamerlars.lua
@@ -4,19 +4,18 @@ return {
 	active = true, -- set to false to disable this script
 	logging = {marker = "timer_actions_lampen_kamerlars"},
 	on = {
-		devices = {
-			'Sw Lamp Lars'
-		},
+		timer = {'at 20:50'}
 	},
-	--on = {
-	--	timer = {'at 20:50'}
-	--},
 	execute = function(domoticz, device)
 		local Time = require('Time')
-		domoticz.devices('Leeslamp Lars').switchOff()
-		domoticz.devices('Single Wall Switch Lamp Lars').switchOff()	
-		domoticz.devices('Leeslamp Lars').switchOn().forSec(20).afterSec(2)
-		domoticz.devices('Leeslamp Lars').switchOff().afterSec(30)
-		domoticz.log('Lars lampen uitgezet',domoticz.LOG_INFO)
+		if domoticz.devices('Leeslamp Lars').state == 'On' then
+			domoticz.devices('Leeslamp Lars').switchOff()
+			domoticz.devices('Single Wall Switch Lamp Lars').switchOff()	
+			domoticz.devices('Leeslamp Lars').switchOn().forSec(20).afterSec(2)
+			domoticz.devices('Leeslamp Lars').switchOff().afterSec(30)
+			domoticz.log('Lars lampen uitgezet',domoticz.LOG_INFO)
+		else
+			domoticz.devices('Single Wall Switch Lamp Lars').switchOff()
+		end
 	end
 }
