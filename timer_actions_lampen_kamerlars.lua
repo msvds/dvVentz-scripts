29bb219f501221c187d8fe01b656af396bfb6f74
msvds <martijnvanderschoot@gmail.com>
2018-09-28 09:52:22 +0200

    Update timer_actions_lampen_kamerlars.lua

--- timer_actions_lampen_kamerlars.lua
+++ timer_actions_lampen_kamerlars.lua
@@ -15,8 +15,8 @@ return {
 		local Time = require('Time')
 		domoticz.devices('Leeslamp Lars').switchOff()
 		domoticz.devices('Single Wall Switch Lamp Lars').switchOff()	
-		domoticz.devices('Leeslamp Lars').switchOn().forSec(2).repeatAfterSec(1, 3)
-		domoticz.devices('Leeslamp Lars').switchOff().afterMin(2)
+		domoticz.devices('Leeslamp Lars').switchOn().forMin(1).afterSec(5)
+		--domoticz.devices('Leeslamp Lars').switchOff().afterMin(2)
 		domoticz.log('Lars lampen uitgezet',domoticz.LOG_INFO)
 	end
 }
