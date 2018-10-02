eab1d4336b5c37d1476187c4f1548f8f0078f970
msvds <martijnvanderschoot@gmail.com>
2018-10-02 18:58:44 +0200

    Update ToonSetPoint.lua

--- ToonSetPoint.lua
+++ ToonSetPoint.lua
@@ -6,7 +6,7 @@ return {
 		}
 	},
 	execute = function(domoticz, device)
-		domoticz.openURL(string.format('http://%s/happ_thermstat?action=setSetpoint&Setpoint=%s', domoticz.variables('UV_ToonIP').value, tonumber(device.SetPoint)*100))
+		domoticz.openURL(string.format('http://%s/happ_thermstat?action=setSetpoint&Setpoint=%s', domoticz.variables('UV_ToonIP').value, tonumber(device.setPoint)*100))
 				domoticz.log('Setting Toon setpoint to '.. device.SetPoint,domoticz.LOG_INFO)
 	end
 }
