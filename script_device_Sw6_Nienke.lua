723f67fcc562364374ee3df65c7b62e6fd76e650
msvds <martijnvanderschoot@gmail.com>
2018-10-10 07:19:27 +0200

    Update script_device_Sw6_Nienke.lua

--- script_device_Sw6_Nienke.lua
+++ script_device_Sw6_Nienke.lua
@@ -10,12 +10,12 @@ return {
 	execute = function(domoticz, device)
 		if (domoticz.time.matchesRule('at 7:00-20:20') or domoticz.time.matchesRule('at 23:00-06:00')) then
 			if device.state == 'Double Click' then
-				--if domoticz.devices('Single Wall Switch Lamp Lars').state == 'On' then
-				--	domoticz.devices('Single Wall Switch Lamp Lars').switchOff()
-				--	domoticz.devices('Leeslamp Lars').switchOn()
+				--if domoticz.devices('Single Wall Switch Lamp Nienke').state == 'On' then
+				--	domoticz.devices('Single Wall Switch Lamp Nienke').switchOff()
+				--	domoticz.devices('Leeslamp Nienke').switchOn()
 				--else
-				--	domoticz.devices('Single Wall Switch Lamp Lars').switchOn()
-				--	domoticz.devices('Leeslamp Lars').switchOff()
+				--	domoticz.devices('Single Wall Switch Lamp Nienke').switchOn()
+				--	domoticz.devices('Leeslamp Nienke').switchOff()
 				--end
 				if domoticz.devices('Leeslamp Nienke').state == 'On' then
 					domoticz.devices('Leeslamp Nienke').switchOff()
