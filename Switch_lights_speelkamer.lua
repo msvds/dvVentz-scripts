1707356d5ab1ab4786a1ca71d5ea429b16d62915
msvds <martijnvanderschoot@gmail.com>
2018-11-05 21:17:12 +0100

    Update Switch_lights_speelkamer.lua

--- Switch_lights_speelkamer.lua
+++ Switch_lights_speelkamer.lua
@@ -7,9 +7,9 @@ return {
 
 	execute = function(domoticz, device)
 		if (domoticz.devices('Lampen Speelkamer').state == 'On') then
-			domoticz.helpers.switch_lights(domoticz,'Speelkamer','Off',0)
-		else
 			domoticz.helpers.switch_lights(domoticz,'Speelkamer','On',0)
+		else
+			domoticz.helpers.switch_lights(domoticz,'Speelkamer','Off',0)
 		end
 	end
 }
