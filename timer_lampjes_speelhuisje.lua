b90ff6d8d6655217be1642345f3c1bd86633a78a
msvds <martijnvanderschoot@gmail.com>
2018-11-27 12:49:31 +0100

    Update timer_lampjes_speelhuisje.lua

--- timer_lampjes_speelhuisje.lua
+++ timer_lampjes_speelhuisje.lua
@@ -10,15 +10,15 @@ return {
 			if (domoticz.devices('Lampjes speelhuisje').lastUpdate.minutesAgo > 3) then
 				domoticz.devices('Lampjes speelhuisje').switchOn().checkFirst()
 			end
-			domoticz.log('Lampjes speelhuisje aangezet,domoticz.LOG_INFO)
+			domoticz.log('Lampjes speelhuisje aangezet',domoticz.LOG_INFO)
 		elseif (domoticz.time.matchesRule('at 7:15-8:00') and domoticz.devices('SomeoneHome').state == 'On') then			
 			if (domoticz.devices('Lampjes speelhuisje').lastUpdate.minutesAgo > 3) then
 				domoticz.devices('Lampjes speelhuisje').switchOn().checkFirst()
 			end
-			domoticz.log('Lampjes speelhuisje aangezet,domoticz.LOG_INFO)
+			domoticz.log('Lampjes speelhuisje aangezet',domoticz.LOG_INFO)
 		else
 			domoticz.devices('Lampjes speelhuisje').switchOff().checkFirst()
-			domoticz.log('Lampjes speelhuisje uitgezet,domoticz.LOG_INFO)
+			domoticz.log('Lampjes speelhuisje uitgezet',domoticz.LOG_INFO)
 		end
 	end
 }
