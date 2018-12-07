2c5dfba1606a8c97c5b9b01f3dbf815a17d80d49
msvds <martijnvanderschoot@gmail.com>
2018-12-07 20:52:34 +0100

    Create werk.lua

---
+++ werk.lua
@@ -0,0 +1,25 @@
+return {
+	active = true, -- set to false to disable this script
+	logging = {marker = "Sfeer"},
+	on = {
+		devices = {'Werk'},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('Sfeer').state == 'On') then
+			--activeer sfeerlichten
+			domoticz.devices('Tradfri - Group - bar').dimTo(100)
+			domoticz.devices('Tradfri - Group - boven tv').dimTo(100)
+			domoticz.devices('Tradfri - Group - keuken').dimTo(100)			
+			domoticz.devices('Tradfri - Group - hal').dimTo(100)
+			domoticz.devices('Tradfri - Group - entree').dimTo(100)
+		else
+			--deactiveer sfeerlichten, terug naar normaal
+			domoticz.devices('Tradfri - Group - bar').dimTo(50)
+			domoticz.devices('Tradfri - Group - boven tv').dimTo(50)
+			domoticz.devices('Tradfri - Group - keuken').dimTo(50)			
+			domoticz.devices('Tradfri - Group - hal').dimTo(50)
+			domoticz.devices('Tradfri - Group - entree').dimTo(50)
+		end
+	end
+}
