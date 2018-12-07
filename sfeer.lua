10685d4092348ed845bfd4063f9588b10c23c811
msvds <martijnvanderschoot@gmail.com>
2018-12-07 20:45:17 +0100

    Create sfeer.lua

---
+++ sfeer.lua
@@ -0,0 +1,25 @@
+return {
+	active = true, -- set to false to disable this script
+	logging = {marker = "Sfeer"},
+	on = {
+		devices = {'Sfeer'},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('Sfeer').state == 'Off') then
+			--activeer sfeerlichten
+			domoticz.devices('Tradfri - Group - bar').dimTo(10)
+			domoticz.devices('Tradfri - Group - boven tv').dimTo(10)
+			domoticz.devices('Tradfri - Group - keuken').dimTo(10)			
+			domoticz.devices('Tradfri - Group - hal').dimTo(10)
+			domoticz.devices('Tradfri - Group - entree').dimTo(10)
+		else
+			--deactiveer sfeerlichten, terug naar normaal
+			domoticz.devices('Tradfri - Group - bar').dimTo(50)
+			domoticz.devices('Tradfri - Group - boven tv').dimTo(50)
+			domoticz.devices('Tradfri - Group - keuken').dimTo(50)			
+			domoticz.devices('Tradfri - Group - hal').dimTo(50)
+			domoticz.devices('Tradfri - Group - entree').dimTo(50)
+		end
+	end
+}
