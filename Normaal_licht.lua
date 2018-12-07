e16f3f43a93d1bda37555b3cbe09e9a5827b2a2b
msvds <martijnvanderschoot@gmail.com>
2018-12-07 21:29:32 +0100

    Create Normaal_licht.lua

---
+++ Normaal_licht.lua
@@ -0,0 +1,33 @@
+return {
+	active = true, -- set to false to disable this script
+	logging = {marker = "Normaal_licht"},
+	on = {
+		devices = {'Normaal licht'},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('Normaal licht').state == 'On') then
+			--activeer Normaal
+			domoticz.devices('Tradfri - Group - bar').dimTo(50)
+			domoticz.devices('Tradfri - Group - boven tv').dimTo(50)
+			domoticz.devices('Tradfri - Group - keuken').dimTo(50)			
+			domoticz.devices('Tradfri - Group - hal').dimTo(50)
+			domoticz.devices('Tradfri - Group - entree').dimTo(50)
+			domoticz.devices('Yeelight Dimmer eetkamer 1').dimTo(50)
+			domoticz.devices('Yeelight Dimmer eetkamer 2').dimTo(50)
+			domoticz.devices('Fel licht').setState('Off').silent()
+			domoticz.devices('Zacht licht').setState('Off').silent()
+		else
+			--deactiveer Normaal, normaal uit
+			domoticz.devices('Tradfri - Group - bar').switchOff()
+			domoticz.devices('Tradfri - Group - boven tv').switchOff()
+			domoticz.devices('Tradfri - Group - keuken').switchOff()		
+			domoticz.devices('Tradfri - Group - hal').switchOff()
+			domoticz.devices('Tradfri - Group - entree').switchOff()
+			domoticz.devices('Yeelight Dimmer eetkamer 1').switchOff()
+			domoticz.devices('Yeelight Dimmer eetkamer 2').switchOff()
+			domoticz.devices('Fel licht').setState('Off').silent()
+			domoticz.devices('Zacht licht').setState('Off').silent()
+		end
+	end
+}
