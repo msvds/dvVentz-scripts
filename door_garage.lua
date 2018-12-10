75b2e542be1858b4f7ccfd859c75bf49b36243c7
msvds <martijnvanderschoot@gmail.com>
2018-12-10 20:16:31 +0100

    Update door_garage.lua

--- door_garage.lua
+++ door_garage.lua
@@ -28,7 +28,7 @@ return {
 		if (domoticz.devices('Garage deur').state == 'Open' and domoticz.devices('Garage deur').lastUpdate.minutesAgo > 30) then
 			if ((domoticz.globalData.OpenC_Deurgarage > Open_timeout_outside and domoticz.globalData.door_garage_message_interval > 0) or (test == true and domoticz.devices('Test Switch').state == 'On')) then				
 				domoticz.helpers.sendnotification(domoticz,'Garage deur alarm','De garage deur is voor meer dan 30 minuten open!')
-				domoticz.helpers.flash_lights(domoticz,domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Lamp boven TV').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,3,3,5)
+				domoticz.helpers.flash_lights(domoticz,domoticz.devices('Schemerlamp deur').state,domoticz.devices('Lamp spoelb keuken').state,domoticz.devices('Tradfri - Group - boven tv').state,domoticz.devices('Schemerlamp bank').state,domoticz.devices('Lamp hal boven').state,3,3,5)
 				domoticz.globalData.door_garage_message_interval = domoticz.globalData.door_garage_message_interval - (message_interval_minutes/script_interval_minutes)
 			end
 		end
