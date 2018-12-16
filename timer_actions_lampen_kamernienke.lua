7173a5b2208575b1825ef89c84d7d14b8ae864f8
msvds <martijnvanderschoot@gmail.com>
2018-12-16 21:03:15 +0100

    Update timer_actions_lampen_kamernienke.lua

--- timer_actions_lampen_kamernienke.lua
+++ timer_actions_lampen_kamernienke.lua
@@ -10,12 +10,12 @@ return {
 		local Time = require('Time')
 		if domoticz.devices('Leeslamp Nienke').state == 'On' then
 			domoticz.devices('Leeslamp Nienke').switchOff()
-			--domoticz.devices('Single Wall Switch Lamp Lars').switchOff()	
+			domoticz.devices('Single Wall Switch Lamp Nienke').switchOff()	
 			domoticz.devices('Leeslamp Nienke').switchOn().forSec(20).afterSec(2)
 			domoticz.devices('Leeslamp Nienke').switchOff().afterSec(30)
 			domoticz.log('Nienke lampen uitgezet',domoticz.LOG_INFO)
 		else
-			--domoticz.devices('Single Wall Switch Lamp Nienke').switchOff()
+			domoticz.devices('Single Wall Switch Lamp Nienke').switchOff()
 		end
 	end
 }
