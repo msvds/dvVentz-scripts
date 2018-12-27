43f4b688cc1ea463bb404c2a246a31626afdc8b7
msvds <martijnvanderschoot@gmail.com>
2018-12-27 13:41:52 +0100

    Update script_device_Switch_Keuken.lua

--- script_device_Switch_Keuken.lua
+++ script_device_Switch_Keuken.lua
@@ -9,44 +9,66 @@ return {
 
 	execute = function(domoticz, device)
 		if device.state == 'Switch 2' then
-			if (domoticz.devices('Tradfri - Group - bar').state == 'Off') then
-				domoticz.devices('Tradfri - Group - bar').switchOn()	
-				domoticz.devices('Tradfri - Group - bar').switchOn().afterSec(2)
-				domoticz.log('Lampen bar aangezet',domoticz.LOG_INFO)
+			if (device.lastUpdate.secondsAgo > 5) then
+				if (domoticz.devices('Tradfri - Group - bar').state == 'Off') then
+					domoticz.devices('Tradfri - Group - bar').switchOn()	
+					domoticz.devices('Tradfri - Group - bar').switchOn().afterSec(2)
+					domoticz.log('Lampen bar aangezet',domoticz.LOG_INFO)
+				else
+					domoticz.devices('Tradfri - Group - bar').switchOff()
+					domoticz.devices('Tradfri - Group - bar').switchOff().afterSec(2)
+					domoticz.log('Lampen bar uitgezet',domoticz.LOG_INFO)
+				end
 			else
 				domoticz.devices('Tradfri - Group - bar').switchOff()
 				domoticz.devices('Tradfri - Group - bar').switchOff().afterSec(2)
 				domoticz.log('Lampen bar uitgezet',domoticz.LOG_INFO)
 			end
-		elseif device.state == 'Switch 1' then
-			if (domoticz.devices('Tradfri - Group - keuken').state == 'Off') then
-				domoticz.devices('Tradfri - Group - keuken').switchOn()
-				domoticz.devices('Tradfri - Group - keuken').switchOn().afterSec(2)
-				domoticz.devices('Lamp spoelb keuken').switchOn()
-				domoticz.log('Lampen keuken aangezet',domoticz.LOG_INFO)
+		elseif device.state == 'Switch 1' then			
+			if (device.lastUpdate.secondsAgo > 5) then
+				if (domoticz.devices('Tradfri - Group - keuken').state == 'Off') then
+					domoticz.devices('Tradfri - Group - keuken').switchOn()
+					domoticz.devices('Tradfri - Group - keuken').switchOn().afterSec(2)
+					domoticz.devices('Lamp spoelb keuken').switchOn()
+					domoticz.log('Lampen keuken aangezet',domoticz.LOG_INFO)
+				else
+					domoticz.devices('Tradfri - Group - keuken').switchOff()
+					domoticz.devices('Tradfri - Group - keuken').switchOff().afterSec(2)
+					domoticz.devices('Lamp spoelb keuken').switchOff()
+					domoticz.log('Lampen keuken uitgezet',domoticz.LOG_INFO)
+				end
 			else
 				domoticz.devices('Tradfri - Group - keuken').switchOff()
 				domoticz.devices('Tradfri - Group - keuken').switchOff().afterSec(2)
 				domoticz.devices('Lamp spoelb keuken').switchOff()
 				domoticz.log('Lampen keuken uitgezet',domoticz.LOG_INFO)
 			end
-			
 		elseif (device.state == 'Both_Click') then
-			if (domoticz.devices('Tradfri - Group - bar').state == 'Off') then
-				domoticz.devices('Tradfri - Group - bar').switchOn()	
-				domoticz.devices('Tradfri - Group - bar').switchOn().afterSec(2)
-				domoticz.log('Lampen bar aangezet',domoticz.LOG_INFO)
+			if (device.lastUpdate.secondsAgo > 5) then
+				if (domoticz.devices('Tradfri - Group - bar').state == 'Off') then
+					domoticz.devices('Tradfri - Group - bar').switchOn()	
+					domoticz.devices('Tradfri - Group - bar').switchOn().afterSec(2)
+					domoticz.log('Lampen bar aangezet',domoticz.LOG_INFO)
+				else
+					domoticz.devices('Tradfri - Group - bar').switchOff()
+					domoticz.devices('Tradfri - Group - bar').switchOff().afterSec(2)
+					domoticz.log('Lampen bar uitgezet',domoticz.LOG_INFO)
+				end
+				if (domoticz.devices('Tradfri - Group - keuken').state == 'Off') then
+					domoticz.devices('Tradfri - Group - keuken').switchOn()
+					domoticz.devices('Tradfri - Group - keuken').switchOn().afterSec(2)
+					domoticz.devices('Lamp spoelb keuken').switchOn()
+					domoticz.log('Lampen keuken aangezet',domoticz.LOG_INFO)
+				else
+					domoticz.devices('Tradfri - Group - keuken').switchOff()
+					domoticz.devices('Tradfri - Group - keuken').switchOff().afterSec(2)
+					domoticz.devices('Lamp spoelb keuken').switchOff()
+					domoticz.log('Lampen keuken uitgezet',domoticz.LOG_INFO)
+				end
 			else
 				domoticz.devices('Tradfri - Group - bar').switchOff()
 				domoticz.devices('Tradfri - Group - bar').switchOff().afterSec(2)
 				domoticz.log('Lampen bar uitgezet',domoticz.LOG_INFO)
-			end
-			if (domoticz.devices('Tradfri - Group - keuken').state == 'Off') then
-				domoticz.devices('Tradfri - Group - keuken').switchOn()
-				domoticz.devices('Tradfri - Group - keuken').switchOn().afterSec(2)
-				domoticz.devices('Lamp spoelb keuken').switchOn()
-				domoticz.log('Lampen keuken aangezet',domoticz.LOG_INFO)
-			else
 				domoticz.devices('Tradfri - Group - keuken').switchOff()
 				domoticz.devices('Tradfri - Group - keuken').switchOff().afterSec(2)
 				domoticz.devices('Lamp spoelb keuken').switchOff()
