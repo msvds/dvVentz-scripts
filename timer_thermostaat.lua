ab7c0dd823696afa64888e2d4d871d08fa7c07f2
msvds <martijnvanderschoot@gmail.com>
2018-12-30 16:33:12 +0100

    Update timer_thermostaat.lua

--- timer_thermostaat.lua
+++ timer_thermostaat.lua
@@ -7,29 +7,29 @@ return {
 	execute = function(domoticz, device)
 		local Time = require('Time')		
 		if (domoticz.time.matchesRule('at 7:20-7:45 on mon,tue,wed,thu,fri') and domoticz.devices('SomeoneHome').state == 'On') then				
-			domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
-			domoticz.helpers.change_heat(domoticz,'Badkamer','Comfort',30)
+			domoticz.helpers.change_heat(domoticz,'Floor1','Home')
+			domoticz.helpers.change_heat(domoticz,'Badkamer','Comfort')
 		end
 		if (domoticz.time.matchesRule('at 8:00-8:05 on mon,thu,fri')) then			
-			domoticz.helpers.change_heat(domoticz,'Inside','Away',0)
+			domoticz.helpers.change_heat(domoticz,'Inside','Away')
 		end
 		if (domoticz.time.matchesRule('at 14:00-14:05 on mon') and domoticz.devices('SomeoneHome').state == 'On') then				
-			domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
+			domoticz.helpers.change_heat(domoticz,'Floor1','Home')
 		end
 		if (domoticz.time.matchesRule('at 17:00-17:05 on mon,tue,wed,fri') and domoticz.devices('SomeoneHome').state == 'On') then				
-			domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
+			domoticz.helpers.change_heat(domoticz,'Floor1','Home')
 		end
 		if (domoticz.time.matchesRule('at 18:00-18:05 on thu') and domoticz.devices('SomeoneHome').state == 'On') then				
-			domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
+			domoticz.helpers.change_heat(domoticz,'Floor1','Home')
 		end
 		if (domoticz.time.matchesRule('at 23:55-23:59')) then				
-			domoticz.helpers.change_heat(domoticz,'Inside','Sleep',0)
+			domoticz.helpers.change_heat(domoticz,'Inside','Sleep')
 		end
 		if (domoticz.time.matchesRule('at 21:15-21:20 on sun,mon,tue,wed,thu')) then				
-			domoticz.helpers.change_heat(domoticz,'Inside','Sleep',0)
+			domoticz.helpers.change_heat(domoticz,'Inside','Sleep')
 		end
 		if (domoticz.time.matchesRule('at 22:15-22:20 on fri,sat')) then				
-			domoticz.helpers.change_heat(domoticz,'Inside','Sleep',0)
+			domoticz.helpers.change_heat(domoticz,'Inside','Sleep')
 		end
 		
 	end
