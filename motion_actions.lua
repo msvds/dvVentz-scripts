2684c2b0b9866807ab7fbdcf1514c3e9b0f26bb8
msvds <martijnvanderschoot@gmail.com>
2018-12-30 13:23:09 +0100

    Update motion_actions.lua

--- motion_actions.lua
+++ motion_actions.lua
@@ -81,10 +81,12 @@ return {
 		if (domoticz.devices('Beweging kamer Lars').state == 'On') then
 			domoticz.log('MC_PIR_kamerLars = ' ..domoticz.globalData.MC_PIR_kamerLars)
 			if (domoticz.globalData.MC_PIR_kamerLars > Heating_On_Minutes and domoticz.devices('Temperatuur Kamer Lars').temperature <= 19) then
-				domoticz.devices('Roomlars-Stat').updateSetPoint(21).forMin(30)
+				domoticz.helpers.change_heat(domoticz,'KamerLars','Comfort',30)
+				domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
+				--domoticz.devices('Roomlars-Stat').updateSetPoint(21).forMin(30)
 				--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
-				domoticz.devices('Toon Scenes').switchSelector(40).forMin(30)
-				domoticz.log('Toon Scenes gezet op Comfort (40) door beweging in kamer Lars de verwarming daar is aangedaan',domoticz.LOG_INFO)
+				--domoticz.devices('Toon Scenes').switchSelector(40).forMin(30)
+				--domoticz.log('Toon Scenes gezet op Comfort (40) door beweging in kamer Lars de verwarming daar is aangedaan',domoticz.LOG_INFO)
 			end
 		end
 		--verwarming
@@ -93,10 +95,12 @@ return {
 			domoticz.log('MC_PIR_kamerNienke = ' ..domoticz.globalData.MC_PIR_kamerNienke)
 			domoticz.log('temp kamerNienke = ' ..domoticz.devices('Temperatuur Kamer Nienke').temperature)
 			if (domoticz.globalData.MC_PIR_kamerNienke > Heating_On_Minutes and domoticz.devices('Temperatuur Kamer Nienke').temperature <= 19) then
-				domoticz.devices('Roomnienke-Stat').updateSetPoint(21).forMin(30)
+				domoticz.helpers.change_heat(domoticz,'KamerNienke','Comfort',30)
+				domoticz.helpers.change_heat(domoticz,'Floor1','Home',0)
+				--domoticz.devices('Roomnienke-Stat').updateSetPoint(21).forMin(30)
 				--0=Off/10=Away/20=Sleep/30=Home/40=Comfort/50=Manual
-				domoticz.devices('Toon Scenes').switchSelector(40).forMin(30)
-				domoticz.log('Toon Scenes gezet op Comfort (40) door beweging in kamer Nienke de verwarming daar is aangedaan',domoticz.LOG_INFO)
+				--domoticz.devices('Toon Scenes').switchSelector(40).forMin(30)
+				--domoticz.log('Toon Scenes gezet op Comfort (40) door beweging in kamer Nienke de verwarming daar is aangedaan',domoticz.LOG_INFO)
 			end
 		end
 	end
