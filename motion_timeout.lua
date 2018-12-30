ef1a9ee29369d81b5df4f1f485a6c48ecacb9832
msvds <martijnvanderschoot@gmail.com>
2018-12-30 16:54:35 +0100

    Update motion_timeout.lua

--- motion_timeout.lua
+++ motion_timeout.lua
@@ -8,21 +8,21 @@ return {
 	},
 	execute = function(domoticz, device)
 		--timeout nacht
-		local NM_timeout_floor1 = 5
+		local NM_timeout_floor1 = 10
 		local NM_timeout_floor2 = 5
 		local NM_timeout_kamerLars = 5
 		local NM_timeout_kamerNienke = 5
 		if (domoticz.time.matchesRule('at 7:00-23:30')) then		
 			if (domoticz.devices('Status').state ~= 50) then
 				--timeout dag
-				NM_timeout_floor1 = 45
+				NM_timeout_floor1 = 30
 				NM_timeout_floor2 = 10
 				NM_timeout_kamerLars = 5
 				NM_timeout_kamerNienke = 5
 			else
 				--timeout dag met gasten
-				NM_timeout_floor1 = 90
-				NM_timeout_floor2 = 30
+				NM_timeout_floor1 = 60
+				NM_timeout_floor2 = 20
 				NM_timeout_kamerLars = 5
 				NM_timeout_kamerNienke = 5
 			end		
@@ -37,13 +37,14 @@ return {
 		--domoticz.log('domoticz.devices('Lamp hal boven').state = ' ..domoticz.devices('Lamp hal boven').state)
 		--Do something when no movement timeout is reached		
 		if (domoticz.globalData.NMC_Floor1 > NM_timeout_floor1) then			
-			--if (domoticz.devices('Sony TV').state == 'Off') then
-			--if (MediaCenter.state == 'Off') then
-			domoticz.helpers.switch_lights(domoticz,'Floor1','Off',3)
-			-- Gateway status resetten
-			domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
-			domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
-			--domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
+			if (domoticz.devices('Sony TV').state == 'Off') then
+				domoticz.helpers.switch_lights(domoticz,'Floor1','Off',3)
+				domoticz.helpers.change_heat(domoticz,'Floor1','Away')
+				-- Gateway status resetten
+				domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
+				domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
+				--domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
+			end
 		end		
 		if (domoticz.globalData.NMC_Floor2 > NM_timeout_floor2) then
 			domoticz.helpers.switch_lights(domoticz,'Floor2','Off',10)
@@ -53,14 +54,14 @@ return {
 			--domoticz.log('Gateway status gereset',domoticz.LOG_INFO)
 		end
 		if (domoticz.globalData.NMC_PIR_kamerLars > NM_timeout_kamerLars) then
-			if (domoticz.devices('eQ-3 - Roomlars-Rad - Thermostat').setPoint ~= '10') then
+			--if (domoticz.devices('eQ-3 - Roomlars-Rad - Thermostat').setPoint ~= '10') then
 				domoticz.helpers.change_heat(domoticz,'KamerLars','Away')
-			end
+			--end
 		end
 		if (domoticz.globalData.NMC_PIR_kamerNienke > NM_timeout_kamerNienke) then
-			if (domoticz.devices('eQ-3 - Roomnienke-Rad - Thermostat').setPoint ~= '10') then
+			--if (domoticz.devices('eQ-3 - Roomnienke-Rad - Thermostat').setPoint ~= '10') then
 				domoticz.helpers.change_heat(domoticz,'KamerNienke','Away')
-			end
+			--end
 		end
 	end
 }
