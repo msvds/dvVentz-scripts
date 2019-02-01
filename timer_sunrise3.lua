856783e9a6333550f82a3a5fc580e2b8791b8a11
msvds <martijnvanderschoot@gmail.com>
2019-02-01 09:09:29 +0100

    Update timer_sunrise3.lua

--- timer_sunrise3.lua
+++ timer_sunrise3.lua
@@ -2,13 +2,12 @@ return {
 	active = true,
 	logging = {marker = "timer_sunrise3"},
 	on = {
-		timer = {'every 1 minute between 40 minutes after sunrise and 55 minutes after sunrise'}
+		timer = {'every 1 minute between 40 minutes after sunrise and 45 minutes after sunrise'}
 	},
 	execute = function(domoticz, device)
-		domoticz.log('Lux woonkamer = ' ..domoticz.devices('Lux woonkamer').lux,domoticz.LOG_INFO)
-		--if (domoticz.devices('Lux woonkamer').value < 3) then
-		--	domoticz.helpers.switch_lights(domoticz,'Inside','Off',3)
-		--end
+		if (domoticz.devices('Lux woonkamer').lux < 3) then
+			domoticz.helpers.switch_lights(domoticz,'Inside','Off',3)
+		end
 		domoticz.log('lampen binnen uitgezet ivm zonsopgang',domoticz.LOG_INFO)
 		-- Gateway status resetten
 		domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
