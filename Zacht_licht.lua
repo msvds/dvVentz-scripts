37ea8f545fe4740447d243166eb99c6c56ec23f2
msvds <martijnvanderschoot@gmail.com>
2019-02-04 17:46:53 +0100

    Update Zacht_licht.lua

--- Zacht_licht.lua
+++ Zacht_licht.lua
@@ -11,8 +11,6 @@ return {
 			domoticz.devices('Tradfri - Group - bar').dimTo(10)
 			domoticz.devices('Tradfri - Group - boven tv').dimTo(10)
 			domoticz.devices('Tradfri - Group - keuken').dimTo(10)			
-			domoticz.devices('Tradfri - Group - hal').dimTo(10)
-			domoticz.devices('Tradfri - Group - entree').dimTo(10)
 			domoticz.devices('Yeelight Dimmer eetkamer 1').dimTo(10)
 			domoticz.devices('Yeelight Dimmer eetkamer 2').dimTo(10)
 			domoticz.devices('Normaal licht').setState('Off').silent()			
