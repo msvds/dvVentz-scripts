aa23197242a7ac7c7ad2b3546d62960bb4c99b78
msvds <martijnvanderschoot@gmail.com>
2019-02-11 16:47:31 +0100

    Create Alarm.lua

---
+++ Alarm.lua
@@ -0,0 +1,17 @@
+return {
+	active = true, -- set to false to disable this script
+	logging = {marker = "Alarm"},
+	on = {
+		devices = {'Alarm'},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('Alarm').state == 'On') then
+			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDAWAY) -- This will  the zone "My Home" to "Armed Away" after the default exit delay
+			domoticz.log('Alarm aangezet',domoticz.LOG_INFO)
+		else
+			alarm.zones('My Home').disArmZone(domoticz)
+			domoticz.log('Alarm uitgezet',domoticz.LOG_INFO)		
+		end
+	end
+}
