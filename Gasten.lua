31d5e4ac6b8c07f770f69f17efe949b36020df16
msvds <martijnvanderschoot@gmail.com>
2019-02-11 16:41:18 +0100

    Create Gasten.lua

---
+++ Gasten.lua
@@ -0,0 +1,18 @@
+return {
+	active = true, -- set to false to disable this script
+	logging = {marker = "Gasten"},
+	on = {
+		devices = {'Gasten'},
+	},
+
+	execute = function(domoticz, device)
+		if (domoticz.devices('Gasten').state == 'On') then
+			domoticz.devices('Status').switchSelector(50) --0=Off/10=Away/20=Holiday/30=Sleep/40=Home/50=Guests/60=Home no notif
+			-- Gateway status resetten
+			domoticz.devices('Xiaomi Gateway Alarm Ringtone eetkamer').switchSelector(0)
+			domoticz.devices('Xiaomi Gateway Alarm Ringtone hal boven').switchSelector(0)
+			domoticz.devices('Gateway light eetkamer').switchSelector(0)--off
+			domoticz.devices('Gateway light hal boven').switchSelector(0)--off
+		end
+	end
+}
