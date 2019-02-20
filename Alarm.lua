e67dd2ffbbb50423b7982a72816c9a1795ed1ec5
msvds <martijnvanderschoot@gmail.com>
2019-02-20 07:46:03 +0100

    Update Alarm.lua

--- Alarm.lua
+++ Alarm.lua
@@ -9,9 +9,11 @@ return {
 		if (domoticz.devices('Alarm').state == 'On') then
 			alarm.zones('My Home').armZone(domoticz, domoticz.SECURITY_ARMEDAWAY) -- This will  the zone "My Home" to "Armed Away" after the default exit delay
 			domoticz.log('Alarm aangezet',domoticz.LOG_INFO)
+			domoticz.devices('Alarm').setState('Off').silent()
 		else
 			alarm.zones('My Home').disArmZone(domoticz)
-			domoticz.log('Alarm uitgezet',domoticz.LOG_INFO)		
+			domoticz.log('Alarm uitgezet',domoticz.LOG_INFO)
+			domoticz.devices('Alarm').setState('Off').silent()
 		end
 	end
 }
