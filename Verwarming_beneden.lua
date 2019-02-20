2d39aef287cb089979c01b73b606a9f6c307aafa
msvds <martijnvanderschoot@gmail.com>
2019-02-20 07:53:06 +0100

    Update Verwarming_beneden.lua

--- Verwarming_beneden.lua
+++ Verwarming_beneden.lua
@@ -8,8 +8,10 @@ return {
 	execute = function(domoticz, device)
 		if (device.state == 'On') then
 			domoticz.helpers.change_heat(domoticz,'Floor1','Home')
+			domoticz.devices('Verwarming beneden').setState('Off').silent()
 		else
 			domoticz.helpers.change_heat(domoticz,'Floor1','Away')
+			domoticz.devices('Verwarming beneden').setState('Off').silent()
 		end
 	end
 }
