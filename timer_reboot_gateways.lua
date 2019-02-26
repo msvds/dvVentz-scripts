969f3092c3005433af65b9e866357d63f953b986
msvds <martijnvanderschoot@gmail.com>
2019-02-26 21:26:44 +0100

    Update timer_reboot_gateways.lua

--- timer_reboot_gateways.lua
+++ timer_reboot_gateways.lua
@@ -7,7 +7,6 @@ return {
 	execute = function(domoticz, device)
 		domoticz.devices('IKEA en EQ-3 MAX gateways').switchOff()
 		domoticz.devices('IKEA en EQ-3 MAX gateways').switchOn().afterSec(15)
-		domoticz.devices('eQ-3 - Chillroom-Rad - Mode').setState('Manual').afterSec(60)
 		domoticz.log('IKEA en EQ-3 MAX gateways gereset',domoticz.LOG_INFO)		
 	end
 }
